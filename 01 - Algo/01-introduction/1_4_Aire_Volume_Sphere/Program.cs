d738e66958cfe4eda523ab92b31584a1b2327194
terencebonhomme <120396084+terencebonhomme@users.noreply.github.com>
2023-04-27 09:36:43 +0200

    add exercise 1.4

---
+++ 01 - Algo/01-introduction/1_4_Aire_Volume_Sphere/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _1_4_Aire_Volume_Sphere
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius = 0, area, volume;
+            string saisie;
+            bool saisieOk;
+
+            do
+            {
+                Console.WriteLine("Saisir le rayon de la sphère avec une virgule si nécessaire");
+                saisie = Console.ReadLine();
+                saisieOk = double.TryParse(saisie, out radius);
+            } while (!saisieOk || radius <= 0);
+
+            area = 4 * Math.PI * Math.Pow(radius, 2);
+            volume = 4 / 3 * Math.PI * Math.Pow(radius, 3);
+
+            Console.WriteLine("l'aire vaut " + area.ToString("#.##") + " et le volume vaut " + volume.ToString("#.##"));
+        }
+    }
+}
---
+++ 01 - Algo/01-introduction/1_4_Aire_Volume_Sphere/pseudo-code.md
@@ -0,0 +1,17 @@
+﻿VARIABLES
+
+	rayon est un flottant
+	aire est un flottant
+	volume est un flottant
+
+TRAITEMENT
+
+	ECRIRE "Saisir le rayon de la sphère"
+	LIRE rayon
+
+	aire <-- 4 * PI * r^2
+	volume <-- 4/3 * PI * r^3
+
+	ECRIRE "L'aire vaut " , aire , " et le volume vaut " , volume
+	
+FIN PROGRAMME
