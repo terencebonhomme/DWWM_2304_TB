2e619b8e3a9e7f39b28d3f49ac1a88a552118e5b
terencebonhomme <120396084+terencebonhomme@users.noreply.github.com>
2023-04-27 10:47:47 +0200

    add exercise 3.6

---
+++ 01 - Algo/03-boucles/3_6_Jeu_Fourchette/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace _3_6_Jeu_Fourchette
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // VARIABLES
+
+            int choice, n, min, max, attempts;
+            string input;
+            bool inputOk;
+            Random rnd;
+
+            // TRAITEMENT
+
+            attempts = 0;
+            min = 0;
+            max = 100;
+
+            rnd = new Random();
+            n = rnd.Next(101);
+
+            do
+            {
+                attempts++;
+
+                do
+                {
+                    Console.Write("Saisir un nombre entier : ");
+                    input = Console.ReadLine();
+                    inputOk = int.TryParse(input, out choice);
+                } while (!inputOk);
+
+                if (choice < n)
+                {
+                    min = choice;
+                }
+                else
+                {
+                    max = choice;
+                }
+
+                if(choice != n)
+                {
+                    Console.WriteLine(min + " - " + max);
+                }
+            } while(choice != n);
+
+            // AFFICHAGE
+
+            Console.WriteLine("Bravo vous avez trouvé en " + attempts + " essais");
+        }
+    }
+}
---
+++ 01 - Algo/03-boucles/3_6_Jeu_Fourchette/pseudo-code.md
@@ -0,0 +1,33 @@
+﻿VARIABLES
+
+	nombreChoisi est un entier
+	n est un entier
+	min est un entier
+	max est un entier
+	nbEssais est un entier
+
+TRAITEMENT
+
+	n <-- RANDOM(0,100)
+
+	ECRIRE "Saisir un nombre"
+	LIRE nombreChoisi
+
+	FAIRE
+		nbEssais <-- nbEssais + 1
+
+		SI nombreChoisi < n ALORS
+			min <-- nombreChoisi
+		SINON SI nombreChoisi > n
+			max <-- nombreChoisi			
+		FIN SI
+
+		SI nombreChoisi != n ALORS
+			ECRIRE min , " - " , max
+		FIN SI
+
+	TANT QUE nombreChoisi != n FIN TANT QUE
+
+AFFICHAGE
+
+	ECRIRE "Bravo vous avez trouvé en " , nbEssais , " essais"
