9fee29d166db2f1fb62562fa6a98ab4aed4c4933
terencebonhomme <120396084+terencebonhomme@users.noreply.github.com>
2023-04-27 16:54:05 +0200

    add exercise 4.3

---
+++ 01 - Algo/04-tableaux/4_3_Denombrer_Alphabet/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _4_3_Denombrer_Alphabet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // VARIABLES
+
+            string text;
+            int counter;
+
+            // TRAITEMENT
+
+            text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sit amet justo egestas, imperdiet mauris ac, tincidunt mi. Aliquam lorem ligula, rhoncus a sagittis a, varius id diam. Maecenas viverra, turpis non ullamcorper varius, turpis velit efficitur risus, in vestibulum eros ante tincidunt tellus. Curabitur congue nisl id ultrices porta. Nullam ultrices nec ligula sit amet accumsan. Nullam elementum elit velit, eget lacinia lectus molestie at. Morbi molestie quis diam sit amet rhoncus. Phasellus luctus erat nec porta ornare. Curabitur eget hendrerit augue.";
+
+            for (int letter = 'a'; letter <= 'z'; letter++)
+            {
+                counter = 0;
+
+                for(int j = 0; j < text.Length; j++)
+                {
+                    if (letter == text[j])
+                    {
+                        counter++;
+                    }
+                }
+
+                Console.WriteLine((char) letter + " : " + counter);
+            }           
+
+            // FIN PROGRAMME
+
+            Console.ReadLine();   
+        }
+    }
+}
---
+++ 01 - Algo/04-tableaux/4_3_Denombrer_Alphabet/pseudo-code.md
@@ -0,0 +1,26 @@
+﻿VARIABLES
+
+	texte est une chaîne de caractères
+	lettre est un entier;
+
+TRAITEMENT
+
+	POUR i de 0 à |texte| - 1 FAIRE
+		
+		POUR lettre de entier de 'a' à entier de 'z' FAIRE
+
+			compteur <-- 0
+
+			SI texte[i] = lettre ALORS
+
+				compteur <-- compteur + 1
+				
+			FIN SI
+
+			ECRIRE lettre , " : " , counter
+
+		FIN POUR
+
+	FIN POUR
+
+AFFICHAGE
