2881789c7a304898344c55daa6b32e4f5cb66e58
terencebonhomme <120396084+terencebonhomme@users.noreply.github.com>
2023-04-27 10:31:57 +0200

    add exercise 2.1

---
+++ 01 - Algo/02-structures-conditionnelles/2_1_Tri_Nombres/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.Cryptography;
+
+namespace _2_1_Tri_Nombres
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // VARIABLES
+
+            int a, b;
+            string input, sort;
+            bool inputOk;
+
+            // TRAITEMENT
+
+            do
+            {
+                Console.WriteLine("Saisir A");
+                input = Console.ReadLine();
+                inputOk = Int32.TryParse(input, out a);
+            } while (!inputOk);
+
+            do
+            {
+                Console.WriteLine("Saisir B");
+                input = Console.ReadLine();
+                inputOk = Int32.TryParse(input, out b);
+            } while (!inputOk);
+
+            sort = a < b ? a + " " + b : b + " " + a;
+
+            // AFFICHAGE
+
+            Console.WriteLine(sort);
+        }
+    }
+}
---
+++ 01 - Algo/02-structures-conditionnelles/2_1_Tri_Nombres/pseudo-code.md
@@ -0,0 +1,21 @@
+﻿VARIABLES
+
+    a est un entier
+    b est un entier
+
+TRAITEMENT
+
+    ECRIRE "Saisir a"
+    LIRE a
+    ECRIRE "Saisir b"
+    LIRE b
+
+AFFICHAGE
+
+    SI a > b ALORS
+        ECRIRE b , " " , a
+    SINON
+        ECRIRE a , " " , b
+    FIN SI
+
+FIN PROGRAMME
