c5140b25118d136ff5d3a82f65a003a1f45e33f2
terencebonhomme <120396084+terencebonhomme@users.noreply.github.com>
2023-04-27 10:44:40 +0200

    add exercise 3.2

---
+++ 01 - Algo/03-boucles/3_2_Saisie_Limite/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _3_2_Saisie_Limite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // VARIABLES
+
+            string input, password, result;
+            int nbAttempts, maxAttempts;
+
+            nbAttempts = 0;
+            maxAttempts = 3;
+            password = "formation";
+
+            // TRAITEMENT
+
+            do
+            {
+                Console.WriteLine("Saisir le mot de passe");
+                input = Console.ReadLine();
+                nbAttempts++;
+            } while (input != password && nbAttempts < maxAttempts);
+
+            if(input == password)
+            {
+                result = "Vous êtes connecté";
+            }
+            else
+            {
+                result = "Votre compte est bloqué";
+            }
+
+            // AFFICHAGE
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 01 - Algo/03-boucles/3_2_Saisie_Limite/pseudo-code.md
@@ -0,0 +1,31 @@
+﻿VARIABLES
+
+	saisie est une chaîne de caractères
+	password est une chaîne de caractères
+	nbEssai est un entier
+	maxEssai est un entier
+
+TRAITEMENT
+
+	password <-- "formation"
+
+	FAIRE
+
+		ECRIRE "Saisir le mot de passe"
+		LIRE saisie
+
+	TANT QUE saisie != password || nbEssai < maxEssai
+
+
+	SI saisie = password ALORS
+
+		ECRIRE "Vous êtes connecté"
+
+	SINON
+
+		ECRIRE "Votre compte est bloqué"
+
+	FIN SI
+
+AFFICHAGE
+
