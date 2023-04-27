c6bdfb4c791c6ff3b49d873c8db9dad36e052f3d
terencebonhomme <120396084+terencebonhomme@users.noreply.github.com>
2023-04-27 10:45:00 +0200

    add exercise 3.3

---
+++ 01 - Algo/03-boucles/3_3_Intervalle_Nombres/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _3_3_Intervalle_Nombres
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // VARIABLES
+
+            int a, b;
+            string input, result;
+            bool inputOk;
+
+            // TRAITEMENT
+
+            do
+            {
+                Console.Write("Saisir a : ");
+                input = Console.ReadLine();
+                inputOk = int.TryParse(input, out a);
+            } while (!inputOk);
+
+            do
+            {
+                Console.Write("Saisir b : ");
+                input = Console.ReadLine();
+                inputOk = int.TryParse(input, out b);
+            } while (!inputOk);
+
+            result = "" + a;
+
+            for(int i = a; i <= b; i++)
+            {
+                result += " " + i;
+            }
+
+            // AFFICHAGE
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 01 - Algo/03-boucles/3_3_Intervalle_Nombres/pseudo-code.md
@@ -0,0 +1,21 @@
+﻿VARIABLES
+
+	a est un entier
+	b est un entier
+	resultat est une chaîne de caractères
+
+TRAITEMENT
+
+	ECRIRE "Saisir a"
+	LIRE a
+
+	ECRIRE "Saisir b"
+	LIRE b
+
+	resultat <-- "" + a
+
+	POUR i de a à b FAIRE
+		resultat <-- resultat, " " ; i
+	FIN POUR
+
+AFFICHAGE
