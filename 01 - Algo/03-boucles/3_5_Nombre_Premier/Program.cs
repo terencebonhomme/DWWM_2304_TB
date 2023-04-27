624e6228eda8e1ba4c8b31d115be8fc6e0f33906
terencebonhomme <120396084+terencebonhomme@users.noreply.github.com>
2023-04-27 10:47:17 +0200

    add exercise 3.5

---
+++ 01 - Algo/03-boucles/3_5_Nombre_Premier/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _3_5_Nombre_Premier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // VARIABLES
+
+            int n;
+            bool isPrime, inputOk;
+            string input;
+
+            // TRAITEMENT
+
+            do
+            {
+                Console.Write("Saisir un nombre entier : ");
+                input = Console.ReadLine();
+                inputOk = int.TryParse(input, out n);
+            } while (!inputOk);
+
+            isPrime = true;
+
+            for(int i = 2; i < n / 2 && isPrime; i++)
+            {
+                if(n % 2 == 0)
+                {
+                    isPrime = false;
+                }
+            }
+
+            // AFFICHAGE
+
+            if (isPrime)
+            {
+                Console.WriteLine(n + " est un nombre premier");
+            }
+            else
+            {
+                Console.WriteLine(n + " n'est pas un nombre premier");
+            }
+        }
+    }
+}
---
+++ 01 - Algo/03-boucles/3_5_Nombre_Premier/pseudo-code.md
@@ -0,0 +1,25 @@
+﻿VARIABLES
+
+	n est un entier
+	isPrime est un booléen
+
+TRAITEMENT
+
+	isPrime <-- vrai
+
+	POUR i de 2 à n / 2 FAIRE
+
+		SI n % 2 = 0 ALORS
+			isPrime <-- faux
+		FIN SI
+
+	FIN POUR
+
+AFFICHAGE
+
+	SI isPrime = vrai ALORS
+		ECRIRE n , " est un nombre premier"
+	SINON
+		ECRIRE n , " n'est un nombre premier"
+	FIN SI
+	
