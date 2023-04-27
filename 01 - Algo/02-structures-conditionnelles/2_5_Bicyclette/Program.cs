ef226d14aa9f500cc4be429e883b9a9be40acaeb
terencebonhomme <120396084+terencebonhomme@users.noreply.github.com>
2023-04-27 10:39:39 +0200

    add exercise 2.5

---
+++ 01 - Algo/02-structures-conditionnelles/2_5_Bicyclette/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace _2_5_Bicyclette
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // VARIABLES
+
+            bool goodWeather;
+            bool bicyclePerfectState;
+            bool immediateFixes;
+            bool GoTInLivingRoom;
+            bool GoTInLibrary;
+
+            string action = "";
+
+            // TRAITEMENT
+
+            Console.WriteLine("\"o\" pour \"oui\" sinon n'importe quelle touche");
+
+            Console.WriteLine("Fera t'il beau ?");
+            goodWeather = Console.ReadLine() == "o";
+
+            if (goodWeather)
+            {
+                Console.WriteLine("Le vélo sera t'il en parfait état ?");
+                bicyclePerfectState = Console.ReadLine() == "o";
+
+                if (bicyclePerfectState)
+                {
+                    action = "Je ferai une balade à bicyclette.";
+                }
+                else
+                {
+                    Console.WriteLine("Je ferai réparer mon vélo.");
+
+                    Console.WriteLine("Les réparations seront-elles immédiates ?");
+                    immediateFixes = Console.ReadLine() == "o";
+
+                    if (immediateFixes)
+                    {
+                        action = "Je ferai une balade à bicyclette.";
+                    }
+                    else
+                    {
+                        action = "J'irai à pied jusqu'à l'étang pour ceuillir les joncs.";
+                    }
+                }                                
+            } 
+            else
+            {
+                Console.WriteLine("J'irai lire un livre.");
+
+                Console.WriteLine("Le tome de Game of Thrones ne sera pas dans le salon ?");
+                GoTInLivingRoom = Console.ReadLine() == "o";
+
+                if (GoTInLivingRoom)
+                {
+                    action = "Je lirai le tome de Game of Thrones du salon dans mon fauteuil.";
+                }
+                else
+                {
+                    Console.WriteLine("J'irai à la bibliothèque.");
+
+                    Console.WriteLine("Le tome de Game of Thrones sera à la bibliothèque ?");
+                    GoTInLibrary = Console.ReadLine() == "o";
+
+                    if (GoTInLibrary)
+                    {
+                        action = "Je lirai le tome de Game of Thrones de la bibliothèque dans mon fauteuil.";
+                    }
+                    else
+                    {
+                        action = "Je lirai un roman policier dans mon fauteuil.";
+                    }
+                }
+            }
+
+            // AFFICHAGE
+
+            Console.WriteLine(action);
+        }
+    }
+}
---
+++ 01 - Algo/02-structures-conditionnelles/2_5_Bicyclette/pseudo-code.md
@@ -0,0 +1,71 @@
+﻿VARIABLES
+	
+	beauTemps est un booléen
+	bicycletteParfaitEtat est un booléen
+	reparationsImmediates est un booléen
+	GoTDansSalon est un booléen
+	GotDansBibliothèque est un booléen
+	action est une chaîne de caractères
+
+TRAITEMENT
+
+	ECRIRE "Fera t'il beau ?"
+	LIRE beauTemps
+
+	SI beauTemps = vrai ALORS
+
+		ECRIRE "J'irai faire une balade."
+
+		ECRIRE "Le vélo sera t'il en parfait état ?"
+		LIRE bicycletteParfaitEtat
+
+		SI bicycletteParfaitEtat ALORS
+
+			action = "Je ferai une balade à bicyclette."
+
+		SINON
+
+			ECRIRE "Je ferai réparer mon vélo."
+
+			ECRIRE "Les réparations seront-elles immédiates ?"
+			LIRE reparationsImmediates
+
+			SI reparationsImmediates ALORS
+				action = "Je ferai une balade à bicyclette."
+			SINON
+				action = "J'irai à pied jusqu'à l'étang pour ceuillir les joncs."
+			FIN SI
+
+		FIN SI
+
+	ELSE
+		
+		ECRIRE "J'irai lire un livre."
+
+		ECRIRE "Le tome de Game of Thrones ne sera pas dans le salon ?"
+		LIRE GoTDansSalon
+
+		SI GoTDansSalon ALORS
+
+			action <-- "Je lirai le tome de Game of Thrones du salon dans mon fauteuil."
+
+		SINON
+
+			ECRIRE "J'irai à la bibliothèque."
+
+			ECRIRE "Le tome de Game of Thrones sera à la bibliothèque ?"
+			LIRE GoTDansBibliotheque
+
+			SI GoTDansBibliotheque ALORS
+				action <-- "Je lirai le tome de Game of Thrones de la bibliothèque dans mon fauteuil."
+			ELSE
+				action <-- "Je lirai un roman policier dans mon fauteuil."
+			FIN SI
+
+		FIN SI
+
+	FIN SI
+
+AFFICHAGE
+
+	ECRIRE action	
