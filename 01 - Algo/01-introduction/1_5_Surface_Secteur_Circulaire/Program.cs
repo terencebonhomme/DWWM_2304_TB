de2ccc4ecee3424fb3dfea11501b371fc9a60d7a
terencebonhomme <120396084+terencebonhomme@users.noreply.github.com>
2023-04-27 09:37:03 +0200

    add exercise 1.5

---
+++ 01 - Algo/01-introduction/1_5_Surface_Secteur_Circulaire/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _1_5_Surface_Secteur_Circulaire
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius = 0, angle = 0, area;
+            string input;
+            bool inputOk;
+
+            do
+            {
+                Console.WriteLine("Saisir le rayon du cercle");
+                input = Console.ReadLine();
+                inputOk = double.TryParse(input, out radius);
+            } while (!inputOk || radius <= 0);
+
+            do
+            {
+                Console.WriteLine("Saisir l'angle du cercle");
+                input = Console.ReadLine();
+                inputOk = double.TryParse(input, out angle);
+            } while (!inputOk || angle <= 0);
+
+            area = Math.PI * Math.Pow(radius, 2) * angle / 360;
+
+            Console.WriteLine("L'aire du secteur circulaire vaut " + area.ToString("#.##"));
+        }
+    }
+}
---
+++ 01 - Algo/01-introduction/1_5_Surface_Secteur_Circulaire/pseudo-code.md
@@ -0,0 +1,18 @@
+﻿VARIABLES
+
+	r est un réel
+	a est un réel
+	aire est un réel
+
+TRAITEMENT
+
+	ECRIRE "Saisir le rayon du cercle"
+	LIRE r
+	ECRIRE "Saisir l'angle du cercle"
+	LIRE a
+
+	aire <-- PI * rayon^2 * a / 360
+
+AFFICHAGE
+
+	ECRIRE "L'aire du secteur circulaire vaut " , aire
