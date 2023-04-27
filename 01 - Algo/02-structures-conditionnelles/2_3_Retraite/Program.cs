bf145f1c113d366c27386286751762e17d564345
terencebonhomme <120396084+terencebonhomme@users.noreply.github.com>
2023-04-27 10:33:14 +0200

    add exercise 2.3

---
+++ 01 - Algo/02-structures-conditionnelles/2_3_Retraite/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _2_3_Retraite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // VARIABLES
+
+            uint a, retirementAge = 60;
+            bool isRetired;
+            string input, result;
+
+            // TRAITEMENT
+
+            do
+            {
+                Console.WriteLine("Saisir votre age");
+                input = Console.ReadLine();
+            } while (!UInt32.TryParse(input, out a));
+
+            if (a >= retirementAge)
+            {
+                Console.WriteLine("Etes-vous à la retraite ?");
+                isRetired = Console.ReadLine().ToLower() == "o";
+
+                if (isRetired)
+                {
+                    result = "Vous êtes à la retraite depuis " + (a - retirementAge) + " années";
+                }
+                else
+                {
+                    result = "C’est le moment de prendre sa retraite.";
+                }
+            }
+            else
+            {
+                result = "Il vous reste " + (retirementAge - a) + " années avant la retraite.";
+            }
+
+            // AFFICHAGE
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 01 - Algo/02-structures-conditionnelles/2_3_Retraite/pseudo-code.md
@@ -0,0 +1,32 @@
+﻿VARIABLES
+
+    a est un entier
+    ageRetraite est un entier
+    estRetraite est un entier
+
+TRAITEMENT
+
+    LIRE a
+
+    SI a >= ageRetraite ALORS
+
+      ECRIRE "Etes-vous à la retraite ?"
+      LIRE "estRetraite"  
+      
+      SI retraite = vrai ALORS
+        ECRIRE "Vous êtes à la retraite depuis " ; a - ageRetraite , " années"
+      SINON
+        ECRIRE "C’est le moment de prendre sa retraite."
+      FIN SI
+      
+    SINON SI a < aRetraite ALORS
+
+      ECRIRE "Il vous reste ", ageRetraite - a ; " années avant la retraite."
+
+    SINON
+
+      "La valeur fournie n’est pas un âge valide."
+
+    FIN SI
+
+FIN PROGRAMME
