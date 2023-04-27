1c7e340086bd48cb7431de10ddcdb6875a498790
terencebonhomme <120396084+terencebonhomme@users.noreply.github.com>
2023-04-27 16:53:00 +0200

    add exercise 4.1

---
+++ 01 - Algo/04-tableaux/4_1_Recherche_Nombre/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _4_1_Recherche_Nombre
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // VARIABLES
+
+            int n, i;
+            string input;
+            bool inputOk, found;
+            int[] exemple; 
+
+            // TRAITEMENT
+            
+            exemple = new int[] {8, 16, 32, 64, 128, 256, 512};
+            found = false;
+
+            do
+            {
+                Console.Write("Saisir n : ");
+                input = Console.ReadLine();
+                inputOk = int.TryParse(input, out n);
+            } while (!inputOk);
+
+            for(i = 0; i < exemple.Length && !found; i++)
+            {
+                if(n == exemple[i])
+                {
+                    found = true;
+                    i--;
+                }
+            }
+
+            // AFFICHAGE
+
+            if (found)
+            {
+                Console.WriteLine(i);
+            }
+            else
+            {
+                Console.WriteLine("Nombre non trouvé");
+            }
+        }
+    }
+}
---
+++ 01 - Algo/04-tableaux/4_1_Recherche_Nombre/pseudo-code.md
@@ -0,0 +1,30 @@
+﻿VARIABLES
+
+	exemple est un tableau d'entiers
+	n est un entier
+	indice est un entier
+	trouve est un booléen
+
+TRAITEMENT
+
+	exemple <-- [8, 16, 32, 64, 128, 256, 512]
+
+	ECRIRE "Saisir n"
+	LIRE n
+	
+	POUR i de 0 à |exemple| - 1 FAIRE
+
+		SI n = exemple[i] ALORS
+			trouve <-- vrai
+			indice <-- i 
+		FIN SI
+
+	FIN POUR
+
+AFFICHAGE
+
+	SI trouve = vrai ALORS
+		ECRIRE indice
+	SINON
+		ECRIRE "Nombre non trouvé"
+	FIN SI
