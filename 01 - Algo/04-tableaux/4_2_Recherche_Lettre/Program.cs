637ff8fbe5b06cb201d9b6b35cf741ffd6c27b07
terencebonhomme <120396084+terencebonhomme@users.noreply.github.com>
2023-04-27 16:53:41 +0200

    add exercise 4.2

---
+++ 01 - Algo/04-tableaux/4_2_Recherche_Lettre/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace _4_2_Recherche_Lettre
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // VARIABLES
+
+            int indice;
+            int occurenceNb;
+            char letter;
+            string str;
+            bool isEmpty;
+
+            // TRAITEMENT
+
+            isEmpty = false;
+            occurenceNb = 0;
+            indice = 0;
+
+            Console.Write("Saisir une phrase : ");
+            str = Console.ReadLine();
+
+            if(str.Length == 0 || str[0] == '.')
+            {
+                Console.WriteLine("LA CHAINE EST VIDE");
+                isEmpty = true;
+            }
+           
+            if (!isEmpty)
+            {
+                Console.Write("Saisir une lettre : ");
+                letter = (char)Console.Read();
+
+                while (str[indice] != '.' && indice < str.Length)
+                {
+                    if (str[indice] == letter)
+                    {
+                        occurenceNb++;
+                    }
+
+                    indice++;
+                }
+
+                Console.WriteLine(occurenceNb);
+            }
+
+            // FIN PROGRAMME
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 01 - Algo/04-tableaux/4_2_Recherche_Lettre/pseudo-code.md
@@ -0,0 +1,32 @@
+﻿VARIABLES
+
+	lettre est un caractère
+	chaine est une chaine de caractères
+	nbOccurences est un entier
+	estVide est un booléen
+
+TRAITEMENT
+
+	i <-- 0
+	nbOccurences <-- 0
+
+	ECRIRE "Saisir une lettre"
+	LIRE lettre
+
+	SI |chaine| = 0 OU chaine[0] = "." ALORS
+		ECRIRE "LA CHAINE EST VIDE"
+		estVide <-- vrai
+	FIN SI
+
+	Si estVide = faux ALORS
+
+		TANT QUE chaine[i] != "." ET i < |chaine| FAIRE
+			SI chaine[i] = lettre ALORS
+				nbOccurences <-- nbOccurences + 1
+			FIN SI	
+			i <-- i + 1
+		FIN TANT QUE
+
+		ECRIRE nbOcurrences
+	FIN SI
+	
