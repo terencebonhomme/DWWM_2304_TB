b8ebde38002d41e3d0f1fae7aed267d35825f26e
terencebonhomme <120396084+terencebonhomme@users.noreply.github.com>
2023-04-27 10:46:43 +0200

    add exercise 3.4

---
+++ 01 - Algo/03-boucles/3_4_Diviseurs_Nombre/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _3_4_Diviseurs_Nombre
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // VARIABLES
+
+            int n;
+            string input;
+            bool inputOk;
+
+            // TRAITEMENT
+
+            do
+            {
+                Console.Write("Saisir un nombre : ");
+                input = Console.ReadLine();
+                inputOk = int.TryParse(input, out n);
+            } while (!inputOk);
+
+            for (int i = 2; i <= n / 2; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ 01 - Algo/03-boucles/3_4_Diviseurs_Nombre/pseudo-code.md
@@ -0,0 +1,17 @@
+﻿VARIABLES
+
+	n est un entier
+
+TRAITEMENT
+
+	LIRE n
+
+	POUR i de 2 à n / 2 ALORS
+		
+		SI n mod 2 = 0 ALORS
+			ECRIRE i
+		FIN SI
+
+	FIN POUR
+
+AFFICHAGE
