4204a5aa939d87515dbe6d8e01d69586512aa67f
terencebonhomme <120396084+terencebonhomme@users.noreply.github.com>
2023-04-27 10:32:43 +0200

    add exercise 2.2

---
+++ 01 - Algo/02-structures-conditionnelles/2_2_Majeur/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace _2_2_Majeur
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // VARIABLES
+
+            uint a;
+            bool inputOk;
+            string result;
+
+            // TRAITEMENT
+
+            do
+            {
+                Console.WriteLine("Saisir votre âge");
+                inputOk = UInt32.TryParse(Console.ReadLine(), out a);
+            } while (!inputOk);
+
+
+            if(a >= 18)
+            {
+                result = "Vous êtes majeur";
+            } 
+            else if(a < 0)
+            {
+                result = "Vous n’êtes pas encore né";
+            } 
+            else
+            {
+                result = "Vous êtes mineur";
+            }
+
+            // AFFICHAGE
+
+            Console.WriteLine(result);                     
+        }
+    }
+}
---
+++ 01 - Algo/02-structures-conditionnelles/2_2_Majeur/pseudo-code.md
@@ -0,0 +1,22 @@
+﻿VARIABLES
+
+	a est un entier
+	conclusion est une chaîne de caractères
+
+TRAITEMENT
+
+	LIRE a
+
+	SI a >= 18 ALORS
+	  conclusion <-- "Vous êtes majeur"
+	SINON SI a < 0
+	  conclusion <-- "Vous n’êtes pas encore né"
+	SINON
+	  conclusion <-- "Vous êtes mineur"
+	FIN SI
+
+CONCLUSION
+	
+	ECRIRE conclusion
+
+FIN PROGRAMME
