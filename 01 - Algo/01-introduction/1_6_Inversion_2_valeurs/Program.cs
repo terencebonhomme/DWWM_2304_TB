b60dd0bb92af19f6baf644d0392df38797d875b1
terencebonhomme <120396084+terencebonhomme@users.noreply.github.com>
2023-04-27 09:37:34 +0200

    add exercise 1.6

---
+++ 01 - Algo/01-introduction/1_6_Inversion_2_valeurs/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _1_6_Inversion_2_valeurs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, temp;
+            string input;
+            bool inputOk;
+
+            do
+            {
+                Console.WriteLine("Saisir A");
+                input = Console.ReadLine();
+                inputOk = int.TryParse(input, out a);
+            } while (!inputOk);
+
+            do
+            {
+                Console.WriteLine("Saisir B");
+                input = Console.ReadLine();
+                inputOk = int.TryParse(input, out b);
+            } while (!inputOk);
+
+            Console.WriteLine(a + " " + b);
+
+            temp = a;
+            a = b;
+            b = temp;
+
+            Console.WriteLine(a + " " + b);
+        }
+    }
+}
---
+++ 01 - Algo/01-introduction/1_6_Inversion_2_valeurs/pseudo-code.md
@@ -0,0 +1,23 @@
+﻿VARIABLES
+
+	a est un entier
+	b est un entier
+
+TRAITEMENT
+
+	LIRE a
+	LIRE b
+
+	ECRIRE a
+	ECRIRE b
+
+	temp <-- a
+	a <-- b
+	b <-- temp
+
+AFFICHAGE
+
+	ECRIRE a
+	ECRIRE b
+
+FIN PROGRAMME
