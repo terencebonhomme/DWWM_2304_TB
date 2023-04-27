f2ca7965ac6a894fab1380b249c1eb482bb92fbc
terencebonhomme <120396084+terencebonhomme@users.noreply.github.com>
2023-04-27 10:42:11 +0200

    add exercise 3.1

---
+++ 01 - Algo/03-boucles/3_1_Controler_Saisie/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _3_1_Controler_Saisie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // VARIABLES
+
+            string firstname, result;
+            int nbAttempts, maxAttempts;
+            bool validResult;
+
+            nbAttempts = 0;
+            maxAttempts = 3;
+            validResult = false;
+            result = "";
+
+            // TRAITEMENT
+
+            do
+            {
+                Console.WriteLine("Saisir votre prenom");
+                firstname = Console.ReadLine();
+                nbAttempts++;
+            } while(firstname.Length < 2 && nbAttempts < maxAttempts);
+
+            if (nbAttempts < maxAttempts)
+            {
+                result = "Bonjour " + firstname;
+            }
+
+            // AFFICHAGE
+
+            if (validResult)
+            {
+                Console.WriteLine(result);
+            }            
+        }
+    }
+}
---
+++ 01 - Algo/03-boucles/3_1_Controler_Saisie/pseudo-code.md
@@ -0,0 +1,23 @@
+﻿VARIABLES
+
+	prenom est une chaîne de caractères
+	nbEssai est un entier
+	maxEssai est un entier
+
+TRAITEMENT
+
+	nbEssai <-- 0
+	maxEssai <-- nbEssai
+
+	FAIRE
+
+		ECRIRE "Saisir votre prenom"
+		LIRE prenom
+		nbEssai <-- nbEssai + 1
+
+	TANT QUE |prenom| < 2 || nbEssai < maxEssai
+
+AFFICHAGE
+
+	SI nbEssai < maxEssai  
+	ECRIRE "Bonjour " , prenom
