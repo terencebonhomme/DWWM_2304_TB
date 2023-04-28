930964752d0e0ada85b021e12c11de819331985f
terencebonhomme <120396084+terencebonhomme@users.noreply.github.com>
2023-04-28 09:29:16 +0200

    add exercise 4.4

---
+++ 01 - Algo/04-tableaux/4_4_Tri_Tableau/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace _4_4_Tri_Tableau
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // VARIABLES
+
+            int[] a;
+            int index = 0;
+            int min;
+            int temp;
+
+            // TRAITEMENT
+
+            a = new int[] { 128, 64, 8, 512, 16, 32, 256 };
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                min = a[i];
+
+                for (int j = i + 1; j < a.Length; j++)
+                {
+                    if (a[j] < min)
+                    {
+                        min = a[j];
+                        index = j;
+                    }
+                }
+
+                temp = a[i];
+                a[i] = a[index];
+                a[index] = temp;                
+            }
+
+            // AFFICHAGE
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write(a[i] + " ");
+            }
+
+            // FIN PROGRAMME
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 01 - Algo/04-tableaux/4_4_Tri_Tableau/pseudo-code.md
@@ -0,0 +1,30 @@
+﻿VARIABLES
+
+	a est un tableau d'entiers
+	i est un entier
+	j est un entier
+	min est un entier
+	indice est intier
+	temp est un entier
+
+TRAITEMENT
+
+	POUR i = 0 à N FAIRE 
+
+		min = a[i];
+
+		POUR j = i + 1 à N FAIRE
+	
+			SI a[i] < min ALORS
+				min <-- a[j]
+				indice <-- j
+			FIN SI
+
+		FIN POUR
+
+		temp <-- a[i]
+		a[i] <-- a[indice]
+		a[indice] <-- temp
+
+	FIN POUR
+
