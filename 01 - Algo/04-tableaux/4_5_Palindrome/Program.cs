d11e88ca977fa06bf3f0de205af8bd1566e97d70
terencebonhomme <120396084+terencebonhomme@users.noreply.github.com>
2023-04-28 11:57:59 +0200

    add exercise 4.5

---
+++ 01 - Algo/04-tableaux/4_5_Palindrome/Program.cs
@@ -0,0 +1,44 @@
+﻿string input;
+string result;
+bool isPalindrome;
+int demiLength;
+
+isPalindrome = true;
+
+do
+{
+    Console.Write("Saisir une chaîne de caractères : ");
+    input = Console.ReadLine().Replace(" ", "");
+
+    demiLength = (int) Math.Floor((input.Length / 2D) - 1);
+
+    for (int i = 0; i < demiLength && isPalindrome; i++)
+    {
+        if (input[i] != input[input.Length - 2 - i])
+        {
+            isPalindrome = false;
+        }
+    }
+} while (input == "." || input.Length > 0 && input[input.Length - 1] != '.');
+
+/*if (input.Length == 0)
+{
+    result = "la phrase est vide";
+}
+else */
+if (isPalindrome)
+{
+    result = "la chaîne de caractères est un palindrome";
+}
+else
+{
+    result = "la chaîne de caractères n’est pas un palindrome";
+}
+
+// AFFICHAGE
+
+Console.WriteLine(result);
+
+// FIN DU PROGRAMME
+
+Console.ReadLine();
---
+++ 01 - Algo/04-tableaux/4_5_Palindrome/pseudo-code.md
@@ -0,0 +1,48 @@
+﻿VARIABLES
+
+	saisie est une chaîne de caractères
+	estPalindrome est un booléen
+	resultat est une chaîne de caractères
+	i est un entier
+
+TRAITEMENT
+
+	estPalindrome <-- vrai
+
+	FAIRE
+
+		ECRIRE "Saisir une chaîne de caractères"
+		LIRE saisie	
+
+		SI |saisie| = 0 ALORS
+
+			resultat <-- "la phrase est vide"
+
+		SINON
+
+			POUR i = 0 à |saisie| - 1 FAIRE
+
+				SI saisie[i] != saisie[|saisie| - 2 - i] ALORS
+					estPalindrome <-- faux
+				FIN SI
+
+			FIN POUR	
+			
+		FIN SI		
+
+	TANT QUE saisie == "." OU saisie[|saisie| - 1] != "."
+
+	SI estPalindrome = vrai ALORS
+
+		resultat <-- "la chaîne de caractères est un palindrome"
+
+	SINON
+
+		resultat <-- "la chaîne de caractères n’est pas un palindrome"
+
+	FIN SI
+
+// AFFICHAGE
+
+	ECRIRE resultat
+	
