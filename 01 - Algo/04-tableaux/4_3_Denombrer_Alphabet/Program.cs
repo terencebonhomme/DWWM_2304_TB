1d9c448d62a08160a9bee891d46e0195f44f80cb
terencebonhomme <120396084+terencebonhomme@users.noreply.github.com>
2023-04-28 08:19:50 +0200

    fix text lowercase

--- 01 - Algo/04-tableaux/4_3_Denombrer_Alphabet/Program.cs
+++ 01 - Algo/04-tableaux/4_3_Denombrer_Alphabet/Program.cs
@@ -13,6 +13,8 @@
 
             text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sit amet justo egestas, imperdiet mauris ac, tincidunt mi. Aliquam lorem ligula, rhoncus a sagittis a, varius id diam. Maecenas viverra, turpis non ullamcorper varius, turpis velit efficitur risus, in vestibulum eros ante tincidunt tellus. Curabitur congue nisl id ultrices porta. Nullam ultrices nec ligula sit amet accumsan. Nullam elementum elit velit, eget lacinia lectus molestie at. Morbi molestie quis diam sit amet rhoncus. Phasellus luctus erat nec porta ornare. Curabitur eget hendrerit augue.";
 
+            text = text.ToLower();
+
             for (int letter = 'a'; letter <= 'z'; letter++)
             {
                 counter = 0;
