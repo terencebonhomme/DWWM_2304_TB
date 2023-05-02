0cbaa0c4bc2c51cfe857d3509115e4fa50229b9e
terencebonhomme <120396084+terencebonhomme@users.noreply.github.com>
2023-05-02 12:10:06 +0200

    add exercise 5.1

---
+++ 01 - Algo/05-revisions1/5_1_Nombres_Parfaits/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace _05_revisions1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // VARIABLES
+
+            int sum;
+            int counter;
+            int number;
+            int[] perfectNumbers = new int[4];
+
+            // TRAITEMENT
+
+            counter = 0;
+            number = 2;
+
+            while(counter < 4)
+            {
+                sum = 0;
+
+                for (int i = 1; i < number; i++)
+                {
+                    if(number % i == 0)
+                    {
+                        sum += i;
+                    }
+                }
+
+                if(sum == number)
+                {
+                    perfectNumbers[counter] = number;
+                    counter++;
+                }
+
+                number++;
+            }
+
+            // AFFICHAGE
+
+            Console.WriteLine("Affichage des 4 premiers nombres parfaits : ");
+
+            for(int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(perfectNumbers[i] + " est un nombre parfait.");
+
+            }
+
+        }
+    }
+}
---
+++ 01 - Algo/05-revisions1/5_1_Nombres_Parfaits/pseudo.code.md
@@ -0,0 +1,42 @@
+﻿VARIABLES
+
+	somme est un entier
+	compteur est un entier
+	nombre est en entier
+	nombreParfaits est un tableau d'entiers
+
+TRAITEMENT
+
+	counter <-- 0
+	nombre <-- 2
+
+	TANT QUE compteur < 4 
+		
+		somme <-- 0
+
+		POUR i de 1 à nombre FAIRE
+			
+			SI nombre mod i = 0 ALORS
+
+				somme <-- somme + i
+
+			FIN SI
+
+		FIN
+
+		SI somme = nombre ALORS			
+			nombresParfaits[compteur] <-- nombre
+			INCREMENTER compteur
+		FIN SI
+
+		INCREMENTER nombre
+
+	FIN TANT QUE	
+
+AFFICHAGE
+
+	ECRIRE "Affichage des 4 premiers nombres parfaits : "
+
+	POUR i de 0 à N FAIRE
+		ECRIRE nombresParfaits[i] , " est un nombre parfait."
+	FIN POUR
