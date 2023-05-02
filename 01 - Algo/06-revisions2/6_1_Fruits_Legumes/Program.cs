b3aadccaaf7416af8e8eaa6db7f9048e41193fcd
terencebonhomme <120396084+terencebonhomme@users.noreply.github.com>
2023-05-02 17:00:20 +0200

    add exercise 6.1

---
+++ 01 - Algo/06-revisions2/6_1_Fruits_Legumes/Program.cs
@@ -0,0 +1,88 @@
+﻿namespace _5_1_Fruits_Legumes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // VARIABLES
+
+            string command;
+            string name;
+            string nameCheapest;
+            double price;                        
+            double priceCheapest;
+            int spacePos;
+            bool quit;
+            bool parseOk;;
+
+            // TRAITEMENT
+
+            quit = false;
+            nameCheapest = "";
+            priceCheapest = double.MaxValue;
+
+            Console.WriteLine("\"nom prix\" par exemple carottes 2.99 pour entrer un légume");
+            Console.WriteLine("\"go\" pour afficher le moins cher");
+            Console.WriteLine("\"help\" pour afficher l'aide");
+            Console.WriteLine("\"quit\" pour quitter");
+            Console.WriteLine();
+
+            do
+            {
+                Console.Write("Saisir une commande : ");
+                command = Console.ReadLine() ?? "";
+
+                if (command == "go")
+                {
+                    if(nameCheapest != "")
+                    {
+                        Console.WriteLine("Légume de moins cher au kilo : " + nameCheapest);
+                    }                    
+                }
+                else if (command == "quit")
+                {
+                    quit = true;
+                }
+                else if(command == "help")
+                {
+                    Console.WriteLine("\"nom prix\" pour entrer un légume (par exemple carottes 2.99)");
+                    Console.WriteLine("\"go\" pour afficher le moins cher");
+                    Console.WriteLine("\"help\" pour afficher l'aide");
+                    Console.WriteLine("\"quit\" pour quitter");
+                }
+                else
+                {
+                    spacePos = -1;
+
+                    for(int i = 0; i < command.Length && spacePos == -1; i++)
+                    {
+                        if (command.ElementAt(i) == ' ')
+                        {
+                            spacePos = i;
+                        }
+                    }
+                    
+                    if(spacePos != -1)
+                    {
+                        name = command.Substring(0, spacePos);
+                        
+                        parseOk = double.TryParse(command.Substring(spacePos + 1), out price);
+
+                        if (parseOk)
+                        {
+                            Console.WriteLine("1 kilogramme de " + name + " coûte " + price + " euros");
+
+                            if (price < priceCheapest)
+                            {
+                                nameCheapest = name;
+                                priceCheapest = price;
+                            }
+                        }
+                    }                    
+                }
+
+                Console.WriteLine();
+            } while (!quit);
+        }
+    }
+}
---
+++ 01 - Algo/06-revisions2/6_1_Fruits_Legumes/pseudo-code.md
@@ -0,0 +1,42 @@
+﻿VARIABLES
+
+	commande est une chaîne de caractères
+	nom est une chaîne de caractères
+	prix est un réel
+	saisie est un tableau de chaîne de caractères
+	nomMoinsCher est un tableau de chaîne de caractères
+	prixMoinsCher est un tableau de réels
+	quitter est un booléen
+
+TRAITEMENT
+
+	quitter <-- false
+
+	FAIRE
+
+		ECRIRE "Saisir par exemple carottes 2.99, go pour afficher le moins cher, quit pour quitter"
+
+		SI commande = "go" ALORS
+					
+			ECRIRE "Légume de moins cher au kilo : " , nomMoinsCher
+
+		SINON SI commande = "quit" ALORS
+
+			quitter <-- vrai
+
+		SINON
+
+			saisie <-- séparer à la position de l'espace
+			nom <-- saisie[0]
+			prix <-- saisie[1]
+
+			ECRIRE "1 kilogramme de " , nom, " coute " , prix , " euros"
+
+			SI prix < prixMoinsCher ALORS
+				nomMoinsCher <-- nom
+				prixMoinsCher <-- prix
+			FIN SI
+
+		FIN SI
+		
+	TANT QUE quitter = faux
