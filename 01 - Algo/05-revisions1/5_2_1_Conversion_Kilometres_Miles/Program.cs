3c156f554705cfdbdbe959366c06df3ee2c1c2c2
terencebonhomme <120396084+terencebonhomme@users.noreply.github.com>
2023-05-02 12:11:18 +0200

    5_2_1_Conversion_Kilometres_Miles/

---
+++ 01 - Algo/05-revisions1/5_2_1_Conversion_Kilometres_Miles/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _5_2_1_Conversion_Kilometres_Miles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // VARIABLES
+
+            double kmDistance;
+            double miDistance;
+            string input;
+            bool inputOk;
+
+            // TRAITEMENT
+            
+            do
+            {
+                Console.Write("Saisir une valeur en kilomètres comprise entre 0.01 et 1 000 000 : ");
+                input = Console.ReadLine();
+                inputOk = double.TryParse(input, out kmDistance);
+            } while (!inputOk);
+
+            miDistance = kmDistance / 1.609;
+
+            // AFFICHAGE
+
+            Console.WriteLine(miDistance);
+        }
+    }
+}
---
+++ 01 - Algo/05-revisions1/5_2_1_Conversion_Kilometres_Miles/pseudo-code.md
@@ -0,0 +1,15 @@
+﻿VARIABLES
+
+	distanceKm est un réel
+	distanceMi est un réel
+
+TRAITEMENT
+
+	ECRIRE "Saisir une valeur en kilomètres comprise entre 0.01 et 1 000 000 : "
+	LIRE distanceKm
+
+	distanceMi <-- distanceKm / 1.609
+
+AFFICHAGE
+
+	ECRIRE distanceMi
