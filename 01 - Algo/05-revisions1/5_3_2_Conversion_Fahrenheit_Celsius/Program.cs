14cbb6626943809f3142be8c989b19ff71a3a124
terencebonhomme <120396084+terencebonhomme@users.noreply.github.com>
2023-05-02 12:13:57 +0200

    add exercise 5.3.2

---
+++ 01 - Algo/05-revisions1/5_3_2_Conversion_Fahrenheit_Celsius/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace _5_3_2_Conversion_Fahrenheit_Celsius
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // VARIABLES
+            
+            string unit;
+            string command;
+            string input;
+            double minimum;
+            double maximum;
+            bool inputOk;
+
+            // TRAITEMENT
+
+            do
+            {
+                Console.Write("Saisir une unité de mesure (C ou F) ou \"quit\" pour quitter : ");
+                command = Console.ReadLine() ?? "";
+
+                if (command == "C" || command == "F")
+                {
+                    unit = command;
+
+                    do
+                    {
+                        Console.Write("Saisir le minimum de la plage de valeurs : ");
+                        input = Console.ReadLine() ?? "";
+                        inputOk = double.TryParse(input, out minimum);
+                    } while (!inputOk);
+
+                    do
+                    {
+                        Console.Write("Saisir le maximum de la plage de valeurs : ");
+                        input = Console.ReadLine() ?? "";
+                        inputOk = double.TryParse(input, out maximum);
+                    } while (!inputOk);
+
+                    if (unit == "F")
+                    {
+                        minimum = (minimum - 32) * 5 / 9;
+                        maximum = (maximum - 32) * 5 / 9;
+                    }
+                    else
+                    {
+                        minimum = (minimum * 9 / 5) + 32;
+                        maximum = (maximum * 9 / 5) + 32;
+                    }
+
+                    Console.WriteLine("[" + String.Format("{0:0.##}", minimum) + ";" 
+                        + String.Format("{0:0.##}", maximum) + "] " + (unit == "C" ? "F" : "C"));
+                }
+            } while (command != "quit");
+        }
+    }
+}
---
+++ 01 - Algo/05-revisions1/5_3_2_Conversion_Fahrenheit_Celsius/pseudo-code.md
@@ -0,0 +1,38 @@
+﻿VARIABLES
+
+	unite est une chaîne de caractères
+	commande est une chaîne de caractères
+	minimum est un réel
+	maximum est un réel	
+
+TRAITEMENT
+
+	FAIRE
+		
+		ECRIRE "Saisir une unité de mesure (C ou F) : "
+		LIRE commande
+
+		SI commande != "quit" ALORS
+
+			unite <-- commande
+
+			ECRIRE "Saisir le minimum de la plage de valeurs  : "
+			LIRE minimum
+
+			ECRIRE "Saisir le maximum de la plage de valeurs : "
+			LIRE maximum
+
+			SI unite = "F" ALORS
+				minimum <-- (minimum - 32) * 5 / 9
+				maximum <-- (maximum - 32) * 5 / 9
+			SINON SI unite = "C" ALORS
+				minimum <-- (minimum * 9 / 5) + 32
+				maximum <-- (maximum * 9 / 5) + 32
+			FIN SI
+
+			ECRIRE "[" , minimum , ";" , maximum, "]"
+
+		FIN SI
+
+	TANT QUE commande != "quit"	
+	
