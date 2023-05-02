ba815abf3f136ffb00331d23b9078481dd39faa1
terencebonhomme <120396084+terencebonhomme@users.noreply.github.com>
2023-05-02 12:13:33 +0200

    add exercise 5.3.1

---
+++ 01 - Algo/05-revisions1/5_3_1_Conversion_Fahrenheit_Celsius/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Formats.Asn1;
+
+namespace _5_3_1_Conversion_Fahrenheit_Celsius
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // VARIABLES
+
+            string temperature;
+            string unit;
+            double value;
+            double result;
+            int spacePos;
+            bool valueOk;
+
+            // TRAITEMENT
+
+            do
+            {
+                Console.Write("Saisir une température suivie de l'unité C/F (exemple 32 C) : ");
+                temperature = Console.ReadLine() ?? "";
+
+                spacePos = 0;
+                unit = "";
+
+                for (int i = 0; i < temperature.Length; i++)
+                {
+                    if (temperature.ElementAt(i) == ' ')
+                    {
+                        spacePos = i;
+                    }
+                }
+
+                valueOk = double.TryParse(temperature.Substring(0, spacePos), out value);
+
+                if(spacePos != 0)
+                {
+                    unit = temperature.Substring(spacePos + 1);
+                }
+
+            } while (!valueOk || (unit != "C" && unit != "F" || unit == "") || (value < -459.67 || value > 5_000_000));
+
+            if(unit == "F")
+            {
+                result = (value - 32) * 5 / 9;
+            }
+            else
+            {
+                result = (value * 9 / 5) + 32;
+            }
+
+            // AFFICHAGE
+
+            Console.WriteLine(String.Format("{0:0.##}", result));
+        }
+    }
+}
---
+++ 01 - Algo/05-revisions1/5_3_1_Conversion_Fahrenheit_Celsius/pseudo-code.md
@@ -0,0 +1,30 @@
+﻿VARIABLES
+
+	temperature est une chaîne de caractères
+	valeur est un réel
+	unite est une chaîne de caractères
+	resultat est un réel
+
+TRAITEMENT
+
+	FAIRE
+
+		ECRIRE "Saisir une température suivie de l'unité C/F (exemple 32 C)"
+		LIRE temperature
+
+		valeur <-- CONVERTIR en réel (temperature de 0 à positionEspace - 1)
+		unite <-- temperature de positionEspace + 1 à |temperature|
+
+	TANT QUE valeur < -459.67 OU valeur > 5 000 000
+
+	SI unite = "F" ALORS
+		resultat <-- (valeur - 32) * 5 / 9
+	SINON SI unite = "C" ALORS
+		resultat <-- (valeur * 9 / 5) + 32
+	FIN SI
+
+AFFICHAGE
+
+	ECRIRE résultat
+
+	
