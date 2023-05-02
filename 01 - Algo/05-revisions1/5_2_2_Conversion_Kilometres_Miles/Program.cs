3347f972edbcd8da1309fd65a5cb0d3d690c14df
terencebonhomme <120396084+terencebonhomme@users.noreply.github.com>
2023-05-02 12:12:48 +0200

    add exercise 5.2.1

---
+++ 01 - Algo/05-revisions1/5_2_2_Conversion_Kilometres_Miles/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace _5_2_2_Conversion_Kilometres_Miles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // VARIABLES
+            
+            string unit;
+            double value;
+            double result;
+            string input;
+            bool inputOk;
+
+            // TRAITEMENT
+
+            result = 0;
+
+            do
+            {
+                Console.Write("Saisir le sens de conversion (km ou mi) : ");
+                unit = Console.ReadLine() ?? "";
+            } while (unit != "km" && unit != "mi" && unit != "");
+
+            if(unit == "")
+            {
+                unit = "km";
+            }
+
+            do
+            {
+                if(unit == "km")
+                {
+                    Console.Write("Saisir une valeur en mi : ");
+                }
+                else
+                {
+                    Console.Write("Saisir une valeur en km : ");
+                }                
+                input = Console.ReadLine() ?? "0";
+                inputOk = double.TryParse(input, out value);
+            } while (!inputOk);
+
+            if (unit == "mi")
+            {
+                result = value / 1.609;
+            }
+            else if (unit == "km")
+            {
+                result = value * 1.609;
+            }            
+
+            // AFFICHAGE
+
+            if(unit == "mi")
+            {
+                Console.WriteLine(result + " mi");
+            }
+            else
+            {
+                Console.WriteLine(result + " km");
+            }            
+        }
+    }
+}
---
+++ 01 - Algo/05-revisions1/5_2_2_Conversion_Kilometres_Miles/pseudo-code.md
@@ -0,0 +1,23 @@
+﻿VARIABLES
+
+	sens est une chaîne de caractères
+	valeur est un réel
+	resultat est un réel
+
+TRAITEMENT
+
+	ECRIRE "Saisir le sens de conversion (km ou mi) : "
+	LIRE sens
+
+	ECRIRE "Saisir une valeur : "
+	LIRE valeur
+
+	SI sens = "mi" ALORS
+		resultat <-- valeur / 1.609
+	SINON SI sens = "km" OU sens = "" ALORS
+		resultat <-- valeur * 1.609		
+	FIN SI
+
+AFFICHAGE
+
+	ECRIRE resultat , " " , sens
