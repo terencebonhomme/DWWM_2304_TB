73d6d78addce4b8d94fd2f4d23a18409b6a9d1af
terencebonhomme <120396084+terencebonhomme@users.noreply.github.com>
2023-05-03 12:20:41 +0200

    add exercise 6.3.1

---
+++ 01 - Algo/06-revisions2/6_3_1_Nombre_Jeunes/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _6_3_1_Nombre_Jeunes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // VARIABLES
+
+            uint age;
+            int counter;
+            string input;
+            bool inputOk;
+
+            // TRAITEMENT
+
+            counter = 0;
+
+            for (int i = 1; i <= 20; i++)
+            {                
+                do
+                {
+                    Console.Write("Saisir la valeur " + i + " : ");
+                    input = Console.ReadLine() ?? "";
+                    inputOk = uint.TryParse(input, out age);
+                } while (!inputOk);
+
+                if(age < 20)
+                {
+                    counter++;
+                }
+            }
+
+            // AFFICHAGE
+
+            Console.WriteLine(counter);
+        }
+    }
+}
---
+++ 01 - Algo/06-revisions2/6_3_1_Nombre_Jeunes/pseudo-code.md
@@ -0,0 +1,23 @@
+﻿VARIABLES
+
+	age est un entier
+	compteur est en entier
+
+TRAITEMENT
+
+	compteur <-- 0
+
+	POUR i de 1 à 20 FAIRE
+
+		ECRIRE "Saisir la valeur " , i , " : "
+		LIRE age
+
+		SI age < 20 ALORS
+			INCREMENTER compteur
+		FIN SI
+
+	FIN POUR
+
+AFFICHAGE
+
+	ECRIRE compteur
