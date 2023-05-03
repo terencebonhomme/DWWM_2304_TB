1d79750aeb35cfd639aa3a51d76f70c3fddb21a8
terencebonhomme <120396084+terencebonhomme@users.noreply.github.com>
2023-05-03 16:48:23 +0200

    add exercise 6.4

---
+++ 01 - Algo/06-revisions2/6_4_Denombrer_Lettres_Alphabet/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace _6_4_Denombrer_Lettres_Alphabet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // VARIABLES
+
+            string text;
+            int consonantNb;
+            int vowelNb;
+            int digitNb;
+            int digitSum;
+            double average;
+
+            // TRAITEMENT
+
+            digitNb = 0;
+            vowelNb = 0;
+            consonantNb = 0;
+            digitSum = 0;            
+            average = 0;
+                       
+            text = "Lorem ipsum dolor6 sit amet,3 consectetur adip7iscing elit. Inte9ger9 vitae interdum5 magna. Etiam mol9estie 2odio et consequat bibendum.";            
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if ("0123456789".Contains(text[i]))
+                {
+                    digitNb++;
+                    digitSum += text[i];
+                } 
+                else if ("aeiouy".Contains(text[i]))
+                {
+                    vowelNb++;
+                }
+                else
+                {
+                    consonantNb++;
+                }
+            }
+
+            if(digitNb > 0)
+            {
+
+                average = (double) digitSum / digitNb;
+            }
+
+            // AFFICHAGE
+
+            Console.WriteLine("Nombre de consonnes : " + consonantNb);
+            Console.WriteLine("Nombre de voyelles : " + vowelNb);
+            Console.WriteLine("Nombre de chiffres : " + digitNb);
+
+            if(digitNb > 0)
+            {
+                Console.WriteLine("Moyenne : " + average);
+            }
+        }
+    }
+}
---
+++ 01 - Algo/06-revisions2/6_4_Denombrer_Lettres_Alphabet/pseudo-code.md
@@ -0,0 +1,45 @@
+﻿VARIABLES
+
+	texte est une chaîne de caractères
+	nbConsonnes est un entier
+	nbVoyelles est un eniter
+	nbChiffres est un entier
+	sommeChiffres est un entier
+	moyenne est un réel
+
+TRAITEMENT
+
+	POUR i de 0 à |texte| FAIRE
+
+		SI "0123456789" contient texte[i] ALORS
+			
+			INCREMENTER nbChiffres
+			sommeChiffres <-- sommeChiffres + texte[i]
+
+		SINON SI "aeiouy" contient texte[i] ALORS
+
+			INCREMENTER nbVoyelles
+
+		SINON	
+
+			INCREMENTER nbConsonnes
+
+		FIN SI
+
+	FIN POUR
+
+	SI nbChiffres > 0 ALORS
+
+		moyenne <-- sommeChiffres / nbChiffres
+
+	FIN SI
+
+AFFICHAGE
+
+	ECRIRE "Nombre de consonnes : " , nbConsonnes
+	ECRIRE "Nombre de voyelles : " , nbVoyelles
+	ECRIRE "Nombre de chiffres : " , nbChiffres
+	
+	SI nbChiffres > 0 ALORS
+		ECRIRE "Moyenne : " , moyenne
+	FIN SI
