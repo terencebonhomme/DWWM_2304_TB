9528c0d64861234b5d5424ff5951cad57c6d9769
terencebonhomme <120396084+terencebonhomme@users.noreply.github.com>
2023-05-09 15:59:15 +0200

    add tp dice

---
+++ 01 - Algo/tp-dice/Program.cs
@@ -0,0 +1,105 @@
+﻿using System.Net.Http.Json;
+
+namespace tp_dice
+{
+    public class Dice
+    {
+        public string? poll { get; set; }
+        public int turns { get; set; }
+        public string[]? results { get; set; }
+    }
+    internal class Program
+    {
+        public static async Task Main()
+        {
+            // VARIABLES
+
+            int[] playerScore;
+            int dice1;
+            int dice2;
+            int sumDices;
+            int playerNum;
+            int? nbTurn;
+            string conclusion;
+            string[]? results;
+            string[] turnResult;            
+            bool parsePlayerNumOk;
+            bool parseDice1Ok;
+            bool parseDice2Ok;
+            bool valid;
+
+            // TRAITEMENT
+
+            // récupérer une partie générée par l'API
+
+            using HttpClient client = new()
+            {
+                BaseAddress = new Uri("https://api.devoldere.net")
+            };
+
+            Dice? dice = await client.GetFromJsonAsync<Dice>("polls/dice");
+
+            nbTurn = dice?.turns;
+            playerNum = 0;
+            playerScore = new int[] { 0, 0, 0 };
+            results = dice?.results;
+
+            valid = results!= null;
+
+            // calculer les scores si on a des résultats
+
+            if (valid)
+            {
+                for (int turn = 0; turn <= nbTurn; turn++)
+                {
+                    // récupérer le joueur et les tirages dans des variables avec le bon type
+
+                    turnResult = results[turn].Split(" ");
+
+                    parsePlayerNumOk = int.TryParse(turnResult[0], out playerNum);
+                    parseDice1Ok = int.TryParse(turnResult[1], out dice1);
+                    parseDice2Ok = int.TryParse(turnResult[2], out dice2);
+
+                    if (parsePlayerNumOk && parseDice1Ok && parseDice2Ok)
+                    {
+                        // calculer les scores en suivant les règles du jeu
+
+                        sumDices = dice1 + dice2;
+
+                        playerNum--;
+
+                        if (dice1 == dice2)
+                        {
+                            if (playerScore[playerNum] >= 2)
+                            {
+                                playerScore[playerNum] -= 2;
+                            }
+                        }
+                        else if (sumDices < 6)
+                        {
+                            playerScore[playerNum] += 0;
+                        }
+                        else if (sumDices <= 10)
+                        {
+                            playerScore[playerNum] += 1;
+                        }
+                        else
+                        {
+                            playerScore[playerNum] += 3;
+                        }
+                    }
+                }
+
+                conclusion = playerScore[0] + "/" + playerScore[1] + "/" + playerScore[2];
+            }
+            else
+            {
+                conclusion = "to investigate";
+            }
+
+            // AFFICHAGE
+
+            Console.WriteLine(conclusion);            
+        }
+    }
+}
---
+++ 01 - Algo/tp-dice/pseudo-code.md
@@ -0,0 +1,49 @@
+﻿VARIABLES
+
+	scoreJoueur est un tableau d'entiers
+	de1 est un entier
+	de2 est un entier
+	somme2Des est un entier
+	numJoueur est un entier
+	resultats est un tableau de chaînes de caractères
+	ngTour est un entier
+
+TRAITEMENT
+
+	nbtour <-- donnée par l'API	
+
+	// calcul des points
+
+	POUR tour de 0 à nbtour FAIRE
+
+		numJoueur <-- donnée par l'API
+		de1 <-- donnée par l'API
+		de2 <-- donnée par l'API
+
+		somme2Des <-- de1 + de2
+		
+		SI de1 = de2 ALORS
+
+			SI scoreJoueur[numJoueur] >= 2 ALORS
+				scoreJoueur[numJoueur] <-- scoreJoueur[numJoueur] - 2
+			FIN SI
+
+		SINON SI somme2Des inférieur à 6 ALORS
+
+			scoreJoueurs[numJoueur] <-- 0
+
+		SINON SI somme2Des entre 6 et 10 ALORS
+
+			scoreJoueurs[numJoueur] <-- 1
+
+		SINON SI somme2Des supérieur à 10 ALORS
+
+			scoreJoueurs[numJoueur] <-- 3
+
+		FIN SI				
+
+	FIN POUR	
+
+AFFICHAGE
+
+	ECRIRE scoreJoueurs[0] , "/" , scoreJoueurs[1] , "/" , scoreJoueurs[2]
