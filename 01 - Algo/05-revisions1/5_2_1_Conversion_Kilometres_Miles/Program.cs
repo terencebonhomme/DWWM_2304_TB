747cf7bf3b8c1f105301f28702331fd6b9792894
terencebonhomme <120396084+terencebonhomme@users.noreply.github.com>
2023-05-12 08:19:12 +0200

    add distance condition

--- 01 - Algo/05-revisions1/5_2_1_Conversion_Kilometres_Miles/Program.cs
+++ 01 - Algo/05-revisions1/5_2_1_Conversion_Kilometres_Miles/Program.cs
@@ -12,19 +12,29 @@
             bool inputOk;
 
             // TRAITEMENT
+
+            miDistance = 0;
             
             do
             {
-                Console.Write("Saisir une valeur en kilomètres comprise entre 0.01 et 1 000 000 : ");
+                Console.Write("Saisir une valeur en kilomètres comprise entre 0,01 et 1 000 000"
+                    + " ou q pour quitter : ");
                 input = Console.ReadLine();
                 inputOk = double.TryParse(input, out kmDistance);
-            } while (!inputOk);
+                Console.WriteLine(inputOk);
+            } while (input != "q" && !inputOk || (kmDistance < 0.01 || kmDistance > 1_000_000));
 
-            miDistance = kmDistance / 1.609;
+            if (input != "q")
+            {
+                miDistance = kmDistance / 1.609;
+            }                 
 
             // AFFICHAGE
 
-            Console.WriteLine(miDistance);
+            if(input != "q")
+            {
+                Console.WriteLine(miDistance);
+            }            
         }
     }
 }
