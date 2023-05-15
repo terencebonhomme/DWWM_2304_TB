c384d4b8783a9d62f33a7444a1eb109fb1bea795
terencebonhomme <120396084+terencebonhomme@users.noreply.github.com>
2023-05-15 12:47:07 +0200

    data structures changed to dictionaries

--- 01 - Algo/tp-yoghurts/Program.cs
+++ 01 - Algo/tp-yoghurts/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Net.Http.Json;
+﻿using System.Collections.Immutable;
+using System.Data;
+using System.Net.Http.Json;
 
 namespace tp_yoghurts
 {
@@ -12,30 +14,25 @@ namespace tp_yoghurts
             bool debug;
             int? nbVotes;
             string[]? results;
-            string firstColor;
-            int max;
+
             int test;
 
-            List<string> maxName;
-            List<int> maxQuantity;
-            List<int> maxPosition;
-            List<string> colorName;
-            List<int> colorPosition;
-            List<int> colorQuantity;
-            List<string> usedColors;
-            List<string> resName;
+            Dictionary<string, int> colorPosition;
+            Dictionary<string, int> colorQuantity;            
+            Dictionary<string, int> firstGroupColorPos;
 
-            int minMaxQuantity;
-            string minMaxName;
-            int minMaxPosition;
-            int tempMaxQuantity;
-            string tempMaxName;
-            int tempMaxPosition;
-            int index;
+            KeyValuePair<string, int> firstColorSample;
+
+            string[] ranking;
+            int rank;
+            string firstColor;
+            int nbRank;
 
             // TRAITEMENT
 
-            // réglages pour les tests
+            // réglages
+
+            nbRank = 2;
 
             test = 0;
             debug = true;
@@ -57,82 +54,85 @@ namespace tp_yoghurts
             // faire le traitement s'il y a des données
             if (valid)
             {
-                colorName = new List<string>();
-                colorPosition = new List<int>();
-                colorQuantity = new List<int>();
+                colorQuantity = new Dictionary<string, int>();
+                colorPosition = new Dictionary<string, int>();
 
                 // utiliser des valeurs de tests qui ne dépendent pas de l'API
                 if (test == 1) 
                 {
-                    colorName.Add("yellow");
-                    colorPosition.Add(4);
-                    colorQuantity.Add(200);
-                    colorName.Add("red");
-                    colorPosition.Add(2);
-                    colorQuantity.Add(500);
-                    colorName.Add("orange");
-                    colorPosition.Add(3);
-                    colorQuantity.Add(500);
-                    colorName.Add("blue");
-                    colorPosition.Add(0);
-                    colorQuantity.Add(500);
-                    colorName.Add("green");
-                    colorPosition.Add(1);
-                    colorQuantity.Add(500);
-                    colorName.Add("pink");
-                    colorPosition.Add(5);
-                    colorQuantity.Add(200);
+                    colorPosition.Add("yellow", 4);
+                    colorQuantity.Add("yellow", 200);
+
+                    colorPosition.Add("red", 2);
+                    colorQuantity.Add("red", 500);
+                    
+                    colorPosition.Add("orange", 3);
+                    colorQuantity.Add("orange", 500);
+                    
+                    colorPosition.Add("blue", 0);
+                    colorQuantity.Add("blue", 500);
+                    
+                    colorPosition.Add("green", 1);
+                    colorQuantity.Add("green", 500);
+                    
+                    colorPosition.Add("pink", 5);
+                    colorQuantity.Add("pink", 200);
                 }
                 else if (test == 2)
                 {
-                    colorName.Add("yellow");
-                    colorPosition.Add(4);
-                    colorQuantity.Add(200);
-                    colorName.Add("red");
-                    colorPosition.Add(2);
-                    colorQuantity.Add(500);
-                    colorName.Add("orange");
-                    colorPosition.Add(1);
-                    colorQuantity.Add(500);
-                    colorName.Add("blue");
-                    colorPosition.Add(0);
-                    colorQuantity.Add(600);
-                    colorName.Add("green");
-                    colorPosition.Add(3);
-                    colorQuantity.Add(500);
-                    colorName.Add("pink");
-                    colorPosition.Add(5);
-                    colorQuantity.Add(200);                    
+                    colorPosition.Add("yellow", 4);
+                    colorQuantity.Add("yellow", 200);
+                    
+                    colorPosition.Add("red", 2);
+                    colorQuantity.Add("red", 500);
+                    
+                    colorPosition.Add("orange", 1);
+                    colorQuantity.Add("orange", 500);
+                    
+                    colorPosition.Add("blue", 0);
+                    colorQuantity.Add("blue", 600);
+                    
+                    colorPosition.Add("green", 3);
+                    colorQuantity.Add("green", 500);
+                    
+                    colorPosition.Add("pink", 5);
+                    colorQuantity.Add("pink", 200);                    
+                }
+                else if (test == 3)
+                {
+                    colorPosition.Add("yellow", 4);
+                    colorQuantity.Add("yellow", 200);
+
+                    colorPosition.Add("red", 2);
+                    colorQuantity.Add("red", 400);
+
+                    colorPosition.Add("orange", 1);
+                    colorQuantity.Add("orange", 200);
+
+                    colorPosition.Add("blue", 0);
+                    colorQuantity.Add("blue", 600);
+
+                    colorPosition.Add("green", 3);
+                    colorQuantity.Add("green", 400);
+
+                    colorPosition.Add("pink", 5);
+                    colorQuantity.Add("pink", 600);
                 }
                 // calculer les votes si on a des résultats
                 else
-                {                    
-                    // ajouter chaque couleur dès la première occurrence
-                    // relever les couleurs utilisées
-
-                    usedColors = new List<string>();
+                {
+                    // compter les couleurs utilisées
 
                     for (int i = 0; i < results?.Length; i++)
                     {
-                        if (!usedColors.Contains(results[i]))                   
+                        if (colorPosition.ContainsKey(results[i]))                   
                         {
-                            usedColors.Add(results[i]);
-                            colorName.Add(results[i]);
-                            colorPosition.Add(i);
-                            colorQuantity.Add(0);
-                        }                    
-                    }
-
-                    // compter le nombre de votes pour chaque couleur
-
-                    for (int i = 0; i < results?.Length; i++)
-                    {
-                        for (int j = 0; j < colorName.Count; j++)
+                            colorQuantity[results[i]]++;                            
+                        }
+                        else
                         {
-                            if (colorName[j] == results[i])
-                            {
-                                colorQuantity[j]++;
-                            }
+                            colorPosition.Add(results[i], i);
+                            colorQuantity.Add(results[i], 1);
                         }
                     }
                 }
@@ -141,157 +141,44 @@ namespace tp_yoghurts
 
                 if (debug)
                 {
-                    for(int i = 0; i < colorName.Count; i++)
+                    foreach (KeyValuePair<string, int> color in colorPosition)
                     {
-                        Console.WriteLine(colorName[i] + " " + colorPosition[i] + " " + colorQuantity[i]);
+                        Console.WriteLine(color.Key + " " + color.Value + " " + colorQuantity[color.Key]);
                     }
                 }
 
-                // créer une liste de valeurs max
+                // classer les valeurs par gropue
 
-                max = int.MinValue;
-                
-                for (int i = 0; i < colorName.Count; i++)
-                {;
-                    if (colorQuantity[i] > max)
-                    {
-                        max = colorQuantity[i];
-                    }
-                }
+                ranking = new string[nbRank];
+                rank = 0;
 
-                maxName = new List<string>();
-                maxQuantity = new List<int>();
-                maxPosition = new List<int>();
-
-                for (int i = 0; i < colorName.Count; i++)
+                while(rank < ranking.Length)
                 {
-                    if (colorQuantity[i] == max)
+                    firstColorSample = colorQuantity.OrderByDescending(c => c.Value).First();
+
+                    firstGroupColorPos = new Dictionary<string, int>();
+
+                    foreach (KeyValuePair<string, int> color in colorQuantity)
                     {
-                        maxName.Add(colorName[i]);
-                        maxQuantity.Add(colorQuantity[i]);
-                        maxPosition.Add(colorPosition[i]);
-                    }                    
-                }
-
-                resName = new List<string>();
-              
-                // trier les valeurs maximales par leur position
-                if (maxName.Count > 1)
-                {
-                    index = 0;
-
-                    for (int i = 0; i < maxPosition.Count; i++)
-                    {
-                        minMaxPosition = maxPosition[i];
-
-                        for (int j = i + 1; j < maxPosition.Count; j++)
+                        if(color.Value == firstColorSample.Value)
                         {
-                            if (maxPosition[j] < minMaxPosition)
-                            {
-                                minMaxPosition = maxPosition[j];
-                                index = j;
-                            }
-                        }
-
-                        tempMaxQuantity = maxQuantity[i];
-                        maxQuantity[i] = maxQuantity[index];
-                        maxQuantity[index] = tempMaxQuantity;
-
-                        tempMaxName = maxName[i];
-                        maxName[i] = maxName[index];
-                        maxName[index] = tempMaxName;
-
-                        tempMaxPosition = maxPosition[i];
-                        maxPosition[i] = maxPosition[index];
-                        maxPosition[index] = tempMaxPosition;
-                    }
-
-                    for (int i = 0; i < maxName.Count; i++)
-                    {
-                        resName.Add(maxName[i]);
-                    }
-                }                
-                // ajouter le max et trier les secondes valeurs maximales par leur position
-                else if (maxName.Count == 1)
-                {
-                    // ajouter le max
-
-                    firstColor = "";                    
-                    max = int.MinValue;
-
-                    for(int i = 0; i < colorName.Count; i++)
-                    {
-                        if (colorQuantity[i] > max)
-                        {
-                            max = colorQuantity[i];
-                            firstColor = colorName[i];
+                            firstGroupColorPos.Add(color.Key, colorPosition[color.Key]);
                         }
                     }
 
-                    resName.Add(firstColor);                                           
-
-                    // comparer si besoin les deuxièmes valeurs maximales
-
-                    max = int.MinValue;
-
-                    for (int i = 0; i < colorName.Count; i++)
-                    {
-                        if (colorQuantity[i] > max && colorName[i] != firstColor)
-                        {
-                            max = colorQuantity[i];
-                        }
-                    }
-
-                    maxName = new List<string>();
-                    maxQuantity = new List<int>();
-                    maxPosition = new List<int>();
-
-                    for (int i = 0; i < colorName.Count; i++)
-                    {
-                        if (colorQuantity[i] == max)
-                        {
-                            maxName.Add(colorName[i]);
-                            maxQuantity.Add(colorQuantity[i]);
-                            maxPosition.Add(colorPosition[i]);
-                        }
-                    }
-
-                    index = 0;
-
-                    for (int i = 0; i < maxPosition.Count; i++)
-                    {
-                        minMaxPosition = maxPosition[i];
-
-                        for (int j = i + 1; j < maxPosition.Count; j++)
-                        {
-                            if (maxPosition[j] < minMaxPosition)
-                            {
-                                minMaxPosition = maxPosition[j];
-                                index = j;
-                            }
-                        }
-
-                        tempMaxQuantity = maxQuantity[i];
-                        maxQuantity[i] = maxQuantity[index];
-                        maxQuantity[index] = tempMaxQuantity;
-
-                        tempMaxName = maxName[i];
-                        maxName[i] = maxName[index];
-                        maxName[index] = tempMaxName;
-
-                        tempMaxPosition = maxPosition[i];
-                        maxPosition[i] = maxPosition[index];
-                        maxPosition[index] = tempMaxPosition;
-                    }
-
-                    for (int i = 0; i < maxName.Count; i++)
-                    {
-                        resName.Add(maxName[i]);
-                    }
+                    firstColor = firstGroupColorPos.OrderBy(c => c.Value).First().Key;
+                    
+                    ranking[rank++] = firstColor;
+                    colorQuantity.Remove(firstColor);
                 }
 
                 // AFFICHAGE;
-                Console.WriteLine(resName.ElementAt(0) + " " + resName.ElementAt(1));
+
+                Console.WriteLine();
+                for (int i = 0; i < ranking.Length; i++)
+                {
+                    Console.WriteLine(ranking[i]);
+                }
             }
             else
             {
