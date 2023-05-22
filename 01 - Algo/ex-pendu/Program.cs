508aa52f248128f07edf942fd513de1ed0850932
terencebonhomme <120396084+terencebonhomme@users.noreply.github.com>
2023-05-22 15:43:08 +0200

    add exercise pendu

---
+++ 01 - Algo/ex-pendu/Program.cs
@@ -0,0 +1,104 @@
+﻿using System.Transactions;
+
+namespace ex_pendu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word;
+            string maskedWord;
+            char letter;
+            int currentPlayer;
+            int winningPlayer;
+            int attemptNb;
+
+            do
+            {
+                Console.Write("Saisir un mot : ");
+                word = Console.ReadLine() ?? "";
+            }
+            while (word.Length < 5);
+
+            maskedWord = maskWord(word);
+            Console.WriteLine(maskedWord);
+
+            currentPlayer = 1;
+            winningPlayer = 0;
+            attemptNb = 0;
+
+            do
+            {
+                Console.Write("joueur " + currentPlayer + " veuillez saisir une lettre : ");
+                letter = Console.ReadLine()[0];
+
+                maskedWord = revealWord(word, maskedWord, letter);
+                Console.WriteLine(maskedWord);
+
+                if(maskedWord != word)
+                {
+                    if(currentPlayer == 2)
+                    {
+                        attemptNb++;
+                    }
+
+                    currentPlayer = changeCurrentPlayer(currentPlayer);
+                }
+                else
+                {
+                    winningPlayer = currentPlayer;
+                }
+            }
+            while (maskedWord != word && attemptNb < 6);
+
+            if(winningPlayer != 0)
+            {
+                Console.WriteLine("joueur " + winningPlayer + " a gagné");
+            }
+            else
+            {
+                Console.WriteLine("personne n'a gagné");
+            }
+            
+        }
+
+        static string revealWord(string word, string maskedWord, char letter)
+        {
+            char[] revealedWord;
+            
+            revealedWord = maskedWord.ToCharArray();
+
+            if (word.ElementAt(0) == letter)
+            {
+                revealedWord[0] = letter;
+            }
+            else if(word.ElementAt(^1) == letter)
+            {
+                revealedWord[^1] = letter;
+            }
+
+            return new string(revealedWord);
+        } 
+
+        static string maskWord(string word)
+        {
+            return "-" + word.Substring(1, word.Length - 2) + "-";
+        }
+
+        static int changeCurrentPlayer(int currentPlayer)
+        {
+            int round;
+
+            if(currentPlayer == 1)
+            {
+                round = 2;
+            }
+            else
+            {
+                round = 1;
+            }
+
+            return round;
+        }
+    }
+}
---
+++ 01 - Algo/ex-pendu/pseudo-code.md
@@ -0,0 +1,80 @@
+﻿VARIABLES
+
+	mot est une chaîne de caractères
+	motMasque est une chaîne de caractères
+	lettre est une chaîne de caractères
+	joueurCourant est un entier
+	joueurGagnant est un entier
+	nbEssai est un entier
+
+TRAITEMENT
+
+	PROGRAMME
+	
+		FAIRE
+			ECRIRE "Saisir un mot : "
+			LIRE mot
+		TANT QUE |mot| < 5
+	
+		motMasque <-- masquerMot(mot)
+		ECRIRE motMasque
+
+		joueurCourant <-- 1
+		joueurGagnant <-- 0
+		nbEssai <-- 1
+
+		FAIRE
+			ECRIRE "joueur " , joueurCourant , " veuillez saisir une lettre : "
+			LIRE lettre
+
+			motMasque <--  revelerLettre(motMasque, lettre)
+
+			ECRIRE motMasque
+
+			SI motMasque != mot ALORS
+				SI joueurCourant = 2 ALORS
+					INCREMENTER nbEssai
+				FIN SI	
+			
+				changerJoueurCourant(joueurCourant)
+			SINON
+				joueurGagnant <-- joueurCourant
+			FIN SI			
+		TANT QUE motMasque != mot ET nbEssai < 6
+
+	FIN PROGRAMME
+
+	chaîne de caractères FONCTION revelerLettre(VAL chaîne de caractère mot, chaîne de caractères motMasque, 
+	caractère lettre)
+		motRevele est une chaîne de caractères
+
+		motRevele <-- mot
+		
+		SI mot(0) = lettre ALORS
+			motRevele(0) <-- lettre
+		SINON SI mot(|mot| - 1) = lettre ALORS
+			motRevele(|mot| - 1) <-- lettre
+		FIN SI
+
+		RETOURNER motRevele
+	FIN FONCTION
+
+	chaîne de caractères FONCTION masquerMot(VAL chaîne de caractères mot)
+		RETOURNER "-" , mot--1-(|mot| - 2) , "-"
+	FIN FONCTION
+
+	entier FONCTION changerJoueurCourant(VAL chaîne de caractères joueurCourant)
+		tour est un entier
+
+		SI joueurCourant = 1 ALORS
+			tour <-- 2
+		SINON
+			tour <-- 1
+		FIN SI
+
+		RETOURNER tour
+	FIN FONCtioN
+
+AFFICHAGE
+	
+	ECRIRE joueurGagnant , " a gagné"
