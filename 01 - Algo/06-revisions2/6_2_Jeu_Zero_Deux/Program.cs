51f5c73de8d7ce2574defb5861db76d7eb387070
terencebonhomme <120396084+terencebonhomme@users.noreply.github.com>
2023-05-22 11:50:07 +0200

    replace if by a switch

--- 01 - Algo/06-revisions2/6_2_Jeu_Zero_Deux/Program.cs
+++ 01 - Algo/06-revisions2/6_2_Jeu_Zero_Deux/Program.cs
@@ -8,6 +8,7 @@
             int playerNumber;
             int cpuScore;
             int playerScore;
+            long diff;
             string input;
             bool inputOk;
             Random rand = new Random();
@@ -26,27 +27,41 @@
                     inputOk = int.TryParse(input, out playerNumber);
                 } while (!inputOk && playerNumber < 0 && playerNumber > 2);
 
-                if(Math.Abs(cpuNumber - playerNumber) == 2)
+                diff = Math.Abs(cpuNumber - playerNumber);
+
+                switch (diff)
                 {
-                    if(cpuNumber > playerNumber)
-                    {
-                        cpuScore++;
-                    }
-                    else
-                    {
-                        playerScore++;
-                    }
-                }
-                else if(Math.Abs(cpuNumber - playerNumber) == 1)
-                {
-                    if (cpuNumber < playerNumber)
-                    {
-                        cpuScore++;
-                    }
-                    else
-                    {
-                        playerScore++;
-                    }
+                    case 2:
+                        if (cpuNumber > playerNumber)
+                        {
+                            cpuScore++;
+                            Console.WriteLine("Ordinateur gagne " + 1 + " point");
+                        }
+                        else
+                        {
+                            playerScore++;
+                            Console.WriteLine("Joueur gagne " + 1 + " point");
+                        }
+                        
+                        break;
+                    case 1:
+                        if (cpuNumber < playerNumber)
+                        {
+                            Console.WriteLine("Ordinateur gagne " + 1 + " point");
+                            cpuScore++;
+                        }
+                        else
+                        {
+                            playerScore++;
+                            Console.WriteLine("Joueur gagne " + 1 + " point");
+                        }
+                        break;
+                    case 0:
+                        Console.WriteLine("Personne gagne de point");
+                        break;
+                    default:
+                        Console.WriteLine("La différence est anormale");
+                        break;
                 }
 
                 Console.WriteLine("Joueur : " + playerScore + " ; Ordinateur : " + cpuScore);
