e95999957b25cd03140bd41d9e88591ad137f79b
terencebonhomme <120396084+terencebonhomme@users.noreply.github.com>
2023-06-12 15:46:40 +0200

    fix and update unit tests

--- 02 - Objets/BouteilleTests/BouteilleTests.cs
+++ 02 - Objets/BouteilleTests/BouteilleTests.cs
@@ -70,10 +70,10 @@ namespace Bouteille.Tests
          */
 
         /// <summary>
-        /// vider une partie d'une bouteille entamée et fermée
+        /// vider une partie d'une bouteille fermée
         /// </summary>
         [TestMethod()]
-        public void ViderPartiellementBouteilleEntameeFermee()
+        public void ViderPartiellementBouteilleFermee()
         {
             const double quantiteDeltaEnCl = 10.0;
             double quantiteAvantEnCl;
@@ -85,40 +85,7 @@ namespace Bouteille.Tests
             Assert.IsFalse(eau.Vider(quantiteDeltaEnCl));
             Assert.IsTrue(eau.QuantiteEnCl == quantiteAvantEnCl);
         }
-
-        /// <summary>
-        /// vider une partie négative d'une bouteille entamée et fermée
-        /// </summary>
-        [TestMethod()]
-        public void ViderPartiellementNegativementBouteilleEntameeFermee()
-        {
-            const double quantiteDeltaEnCl = -10.0;
-            double quantiteAvantEnCL;
-
-            Bouteille eau = new Bouteille("eau", true, 100.0, 150.0);
-
-            quantiteAvantEnCL = eau.QuantiteEnCl;
-
-            Assert.IsFalse(eau.Vider(quantiteDeltaEnCl));
-            Assert.IsTrue(eau.QuantiteEnCl == quantiteAvantEnCL);
-        }
-
-        /// <summary>
-        /// vider toute une bouteille entamée et fermée
-        /// </summary>
-        [TestMethod()]
-        public void ViderTotalementBouteilleEntameeFermee()
-        {
-            double quantiteAvantEnCL;
-
-            Bouteille eau = new Bouteille("eau", false, 100.0, 150.0);
-
-            quantiteAvantEnCL = eau.QuantiteEnCl;
-
-            Assert.IsFalse(eau.Vider());
-            Assert.IsTrue(eau.QuantiteEnCl == quantiteAvantEnCL);
-        }
-
+        
         /// <summary>
         /// vider une partie d'une bouteille entamée et ouverte
         /// </summary>
@@ -137,12 +104,29 @@ namespace Bouteille.Tests
         }
 
         /// <summary>
+        /// vider une partie nulle d'une bouteille entamée et ouverte
+        /// </summary>
+        [TestMethod()]
+        public void ViderPartiellementNullementBouteilleEntameeOuverte()
+        {
+            const double quantiteDeltaEnCl = 0.0;
+            double quantiteAvantEnCl;
+
+            Bouteille eau = new Bouteille("eau", true, 100.0, 150.0);
+
+            quantiteAvantEnCl = eau.QuantiteEnCl;
+
+            Assert.IsFalse(eau.Vider(quantiteDeltaEnCl));
+            Assert.IsTrue(eau.QuantiteEnCl == quantiteAvantEnCl);
+        }
+
+        /// <summary>
         /// vider une partie excessive la totalité d'une bouteille entamée et ouverte
         /// </summary>
         [TestMethod()]
         public void ViderPartiellementTotaliteBouteilleEntameeOuverte()
         {
-            const double quantiteDeltaEnCl = 10.0;
+            const double quantiteDeltaEnCl = 100.0;
             double quantiteAvantEnCl;
 
             Bouteille eau = new Bouteille("eau", true, 100.0, 150.0);
@@ -154,7 +138,7 @@ namespace Bouteille.Tests
         }
 
         /// <summary>
-        /// vider une partie excessive d'une bouteille entamée et ouverte
+        /// vider une partie excessive d'une bouteille et ouverte
         /// </summary>
         [TestMethod()]
         public void ViderPartiellementExcessivementBouteilleEntameeOuverte()
@@ -188,6 +172,22 @@ namespace Bouteille.Tests
         }
 
         /// <summary>
+        /// vider totalement une bouteille entamée et fermée
+        /// </summary>
+        [TestMethod()]
+        public void ViderTotalementBouteilleEntameeFermee()
+        {
+            double quantiteAvantEnCl;
+
+            Bouteille eau = new Bouteille("eau", false, 100.0, 150.0);
+
+            quantiteAvantEnCl = eau.QuantiteEnCl;
+
+            Assert.IsFalse(eau.Vider());
+            Assert.IsTrue(eau.QuantiteEnCl == quantiteAvantEnCl);
+        }
+
+        /// <summary>
         /// vider totalement une bouteille entamée et ouverte
         /// </summary>
         [TestMethod()]
@@ -209,73 +209,6 @@ namespace Bouteille.Tests
          */
 
         /// <summary>
-        /// vider une partie d'une bouteille pleine et fermée
-        /// </summary>
-        [TestMethod()]
-        public void ViderPartiellementBouteillePleineFermee()
-        {
-            const double quantiteDeltaEnCl = 10.0;
-            double quantiteAvantEnCl;
-
-            Bouteille eau = new Bouteille("eau", false, 150.0, 150.0);
-
-            quantiteAvantEnCl = eau.QuantiteEnCl;
-
-            Assert.IsFalse(eau.Vider(quantiteDeltaEnCl));
-            Assert.IsTrue(eau.QuantiteEnCl == quantiteAvantEnCl);
-        }
-
-        /// <summary>
-        /// vider une partie négative d'une bouteille pleine et fermée
-        /// </summary>
-        [TestMethod()]
-        public void ViderPartiellementNegativementBouteillePleineFermee()
-        {
-            const double quantiteDeltaEnCl = -10.0;
-            double quantiteAvantEnCL;
-
-            Bouteille eau = new Bouteille("eau", false, 150.0, 150.0);
-
-            quantiteAvantEnCL = eau.QuantiteEnCl;
-
-            Assert.IsFalse(eau.Vider(quantiteDeltaEnCl));
-            Assert.IsTrue(eau.QuantiteEnCl == quantiteAvantEnCL);
-        }
-
-        /// <summary>
-        /// vider toute une bouteille pleine et fermée
-        /// </summary>
-        [TestMethod()]
-        public void ViderTotalementBouteillePleineFermee()
-        {
-            double quantiteAvantEnCL;
-
-            Bouteille eau = new Bouteille("eau", false, 150.0, 150.0);
-
-            quantiteAvantEnCL = eau.QuantiteEnCl;
-
-            Assert.IsFalse(eau.Vider());
-            Assert.IsTrue(eau.QuantiteEnCl == quantiteAvantEnCL);
-        }
-
-        /// <summary>
-        /// vider une partie d'une bouteille pleine et ouverte
-        /// </summary>
-        [TestMethod()]
-        public void ViderPartiellementBouteillePleineOuverte()
-        {
-            const double quantiteDeltaEnCl = 10.0;
-            double quantiteAvantEnCl;
-
-            Bouteille eau = new Bouteille("eau", true, 150.0, 150.0);
-
-            quantiteAvantEnCl = eau.QuantiteEnCl;
-
-            Assert.IsTrue(eau.Vider(quantiteDeltaEnCl));
-            Assert.IsFalse(eau.QuantiteEnCl == quantiteAvantEnCl);
-        }
-
-        /// <summary>
         /// vider une partie la totalité d'une bouteille pleine et ouverte
         /// </summary>
         [TestMethod()]
@@ -290,23 +223,6 @@ namespace Bouteille.Tests
 
             Assert.IsTrue(eau.Vider(quantiteDeltaEnCl));
             Assert.IsFalse(eau.QuantiteEnCl == quantiteAvantEnCl);
-        }
-
-        /// <summary>
-        /// vider une partie excessive d'une bouteille pleine et ouverte
-        /// </summary>
-        [TestMethod()]
-        public void ViderPartiellementExcessivementBouteillePleineOuverte()
-        {
-            const double quantiteDeltaEnCl = 150.1;
-            double quantiteAvantEnCl;
-
-            Bouteille eau = new Bouteille("eau", true, 150.0, 150.0);
-
-            quantiteAvantEnCl = eau.QuantiteEnCl;
-
-            Assert.IsFalse(eau.Vider(quantiteDeltaEnCl));
-            Assert.IsTrue(eau.QuantiteEnCl == quantiteAvantEnCl);
         }
 
         /// <summary>
@@ -345,58 +261,7 @@ namespace Bouteille.Tests
 
         /**
          * Vider > bouteille vide
-         */
-
-        /// <summary>
-        /// vider une partie d'une bouteille vide et fermée
-        /// </summary>
-        [TestMethod()]
-        public void ViderPartiellementBouteilleVideFermee()
-        {
-            const double quantiteDeltaEnCl = 10.0;
-            double quantiteAvantEnCl;
-
-            Bouteille eau = new Bouteille("eau", false, 0.0, 150.0);
-
-            quantiteAvantEnCl = eau.QuantiteEnCl;
-
-            Assert.IsFalse(eau.Vider(quantiteDeltaEnCl));
-            Assert.IsTrue(eau.QuantiteEnCl == quantiteAvantEnCl);
-        }
-
-        /// <summary>
-        /// vider une partie négative d'une bouteille vide et fermée
-        /// </summary>
-        [TestMethod()]
-        public void ViderPartiellementNegativementBouteilleVideFermee()
-        {
-            const double quantiteDeltaEnCl = -10.0;
-            double quantiteAvantEnCL;            
-
-            Bouteille eau = new Bouteille("eau", false, 0.0, 150.0);
-
-            quantiteAvantEnCL = eau.QuantiteEnCl;
-
-            Assert.IsFalse(eau.Vider(quantiteDeltaEnCl));
-            Assert.IsTrue(eau.QuantiteEnCl == quantiteAvantEnCL);
-        }
-
-        /// <summary>
-        /// vider toute une bouteille vide et fermée
-        /// </summary>
-        [TestMethod()]
-        public void ViderTotalementBouteilleVideFermee()
-        {
-            double quantiteAvantEnCL;
-
-            Bouteille eau = new Bouteille("eau", false, 0.0, 150.0);
-
-            quantiteAvantEnCL = eau.QuantiteEnCl;
-
-            Assert.IsFalse(eau.Vider());
-            Assert.IsTrue(eau.QuantiteEnCl == quantiteAvantEnCL);
-            Assert.IsTrue(eau.QuantiteEnCl == 0);
-        }
+         */      
 
         /// <summary>
         /// vider une partie d'une bouteille vide et ouverte
@@ -416,39 +281,22 @@ namespace Bouteille.Tests
         }
 
         /// <summary>
-        /// vider une partie excessive la totalité d'une bouteille entamée et ouverte
+        /// vider la totalité d'une bouteille entamée et ouverte
         /// </summary>
         [TestMethod()]
         public void ViderPartiellementTotaliteBouteilleVideOuverte()
         {
-            const double quantiteDeltaEnCl = 10.0;
+            const double quantiteDeltaEnCl = 50.0;
             double quantiteAvantEnCl;
 
-            Bouteille eau = new Bouteille("eau", true, 0.0, 150.0);
+            Bouteille eau = new Bouteille("eau", true, 100.0, 150.0);
 
             quantiteAvantEnCl = eau.QuantiteEnCl;
 
-            Assert.IsFalse(eau.Vider(quantiteDeltaEnCl));
-            Assert.IsTrue(eau.QuantiteEnCl == quantiteAvantEnCl);
+            Assert.IsTrue(eau.Vider(quantiteDeltaEnCl));
+            Assert.IsFalse(eau.QuantiteEnCl == quantiteAvantEnCl);
         }
-
-        /// <summary>
-        /// vider une partie excessive d'une bouteille entamée et ouverte
-        /// </summary>
-        [TestMethod()]
-        public void ViderPartiellementExcessivementBouteilleVideOuverte()
-        {
-            const double quantiteDeltaEnCl = 150.1;
-            double quantiteAvantEnCl;
-
-            Bouteille eau = new Bouteille("eau", true, 0.0, 150.0);
-
-            quantiteAvantEnCl = eau.QuantiteEnCl;
-
-            Assert.IsFalse(eau.Vider(quantiteDeltaEnCl));
-            Assert.IsTrue(eau.QuantiteEnCl == quantiteAvantEnCl);
-        }
-
+        
         /// <summary>
         /// vider une partie négative d'une bouteille vide et ouverte
         /// </summary>
@@ -492,7 +340,7 @@ namespace Bouteille.Tests
          */
 
         /// <summary>
-        /// remplir partiellement une bouteille entamee et fermee
+        /// remplir partiellement une bouteille entamée et fermee
         /// </summary>
         [TestMethod()]
         public void RemplirPartiellementBouteilleEntameeFermee()
@@ -507,24 +355,7 @@ namespace Bouteille.Tests
             Assert.IsFalse(eau.Remplir(quantiteDeltaEnCl));
             Assert.IsTrue(eau.QuantiteEnCl == quantiteAvantEnCl);
         }
-
-        /// <summary>
-        /// remplir partiellement avec une valeur négative une bouteille entamee et fermée
-        /// </summary>
-        [TestMethod()]
-        public void RemplirPartiellementNegativementBouteilleEntameeFermee()
-        {
-            const double _quantiteAjoutee = -10.0;
-            double quantiteAvantEnCl;
-
-            Bouteille eau = new Bouteille("eau", false, 100.0, 150.0);
-
-            quantiteAvantEnCl = eau.QuantiteEnCl;
-
-            Assert.IsFalse(eau.Remplir(_quantiteAjoutee));
-            Assert.IsTrue(eau.QuantiteEnCl == quantiteAvantEnCl);
-        }
-
+        
         /// <summary>
         /// remplir partiellement une bouteille entamée et ouverte
         /// </summary>
@@ -598,40 +429,6 @@ namespace Bouteille.Tests
          */
 
         /// <summary>
-        /// remplir partiellement avec une valeur positive une bouteille pleine et fermée
-        /// </summary>
-        [TestMethod()]
-        public void RemplirPartiellementBouteilleFermeePleine()
-        {
-            const double quantiteDeltaEnCl = 10.0;
-            double quantiteAvantEnCl;
-
-            Bouteille eau = new Bouteille("eau", false, 150.0, 150.0);
-
-            quantiteAvantEnCl = eau.QuantiteEnCl;
-
-            Assert.IsFalse(eau.Remplir(quantiteDeltaEnCl));
-            Assert.IsTrue(eau.QuantiteEnCl == quantiteAvantEnCl);
-        }
-
-        /// <summary>
-        /// remplir partiellement avec une valeur négative une bouteille pleine et fermée
-        /// </summary>
-        [TestMethod()]
-        public void RemplirPartiellementNegativementBouteilleFermeePleine()
-        {
-            const double quantiteDeltaEnCl = -10.0;
-            double quantiteAvantEnCl;
-
-            Bouteille eau = new Bouteille("eau", false, 150.0, 150.0);
-
-            quantiteAvantEnCl = eau.QuantiteEnCl;
-
-            Assert.IsFalse(eau.Remplir(quantiteDeltaEnCl));
-            Assert.IsTrue(eau.QuantiteEnCl == quantiteAvantEnCl);
-        }
-
-        /// <summary>
         /// remplir partiellement une bouteille pleine et ouverte
         /// </summary>
         [TestMethod()]
@@ -665,77 +462,9 @@ namespace Bouteille.Tests
             Assert.IsTrue(eau.QuantiteEnCl == quantiteAvantEnCl);
         }
 
-        /// <summary>
-        /// remplir totalement une bouteille pleine et fermée
-        /// </summary>
-        [TestMethod()]
-        public void RemplirTotalementBouteilleFermeePleine()
-        {
-            double quantiteAvantEnCl;
-
-            Bouteille eau = new Bouteille("eau", false, 150.0, 150.0);
-
-            quantiteAvantEnCl = eau.QuantiteEnCl;
-
-            Assert.IsFalse(eau.Remplir());
-            Assert.IsTrue(eau.QuantiteEnCl == quantiteAvantEnCl);
-            Assert.IsTrue(eau.QuantiteEnCl == eau.CapaciteEnCl);
-        }
-
-        /// <summary>
-        /// remplir totalement une bouteille pleine et fermée
-        /// </summary>
-        [TestMethod()]
-        public void RemplirTotalementBouteilleOuvertePleine()
-        {
-            double quantiteAvantEnCl;
-
-            Bouteille eau = new Bouteille("eau", true, 150.0, 150.0);
-
-            quantiteAvantEnCl = eau.QuantiteEnCl;
-
-            Assert.IsFalse(eau.Remplir());
-            Assert.IsTrue(eau.QuantiteEnCl == quantiteAvantEnCl);
-            Assert.IsTrue(eau.QuantiteEnCl == eau.CapaciteEnCl);
-        }
-
         /**
          * Vider > bouteille vide
          */
-
-        /// <summary>
-        /// remplir partiellement avec une valeur positive une bouteille vide et fermée
-        /// </summary>
-        [TestMethod()]
-        public void RemplirPartiellementBouteilleVideFermee()
-        {
-            const double quantiteDeltaEnCl = 10.0;
-            double quantiteAvantEnCl;
-
-            Bouteille eau = new Bouteille("eau", false, 0.0, 150.0);
-
-            quantiteAvantEnCl = eau.QuantiteEnCl;
-
-            Assert.IsFalse(eau.Remplir(quantiteDeltaEnCl));
-            Assert.IsTrue(eau.QuantiteEnCl == quantiteAvantEnCl);
-        }
-
-        /// <summary>
-        /// remplir partiellement avec une valeur négative une bouteille vide et fermée
-        /// </summary>
-        [TestMethod()]
-        public void RemplirPartiellementNegativementBouteilleVideFermee()
-        {
-            const double quantiteDeltaEnCl = -10.0;
-            double quantiteAvantEnCl;
-
-            Bouteille eau = new Bouteille("eau", true, 0.0, 150.0);
-
-            quantiteAvantEnCl = eau.QuantiteEnCl;
-
-            Assert.IsFalse(eau.Remplir(quantiteDeltaEnCl));
-            Assert.IsTrue(eau.QuantiteEnCl == quantiteAvantEnCl);
-        }
 
         /// <summary>
         /// remplir partiellement une bouteille vide et ouverte
@@ -769,23 +498,6 @@ namespace Bouteille.Tests
 
             Assert.IsFalse(eau.Remplir(quantiteDeltaEnCl));
             Assert.IsTrue(eau.QuantiteEnCl == quantiteAvantEnCl);
-        }
-
-        /// <summary>
-        /// remplir totalement une bouteille vide et fermée
-        /// </summary>
-        [TestMethod()]
-        public void RemplirTotalementBouteilleVideFermee()
-        {
-            double quantiteAvantEnCl;
-
-            Bouteille eau = new Bouteille("eau", false, 0.0, 150.0);
-
-            quantiteAvantEnCl = eau.QuantiteEnCl;
-
-            Assert.IsFalse(eau.Remplir());
-            Assert.IsTrue(eau.QuantiteEnCl == quantiteAvantEnCl);
-            Assert.IsFalse(eau.QuantiteEnCl == eau.CapaciteEnCl);
         }
 
         /// <summary>
